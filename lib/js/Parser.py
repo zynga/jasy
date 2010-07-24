707353125a10aef8227daccc65402b0e09db0228
Sebastian Werner <info@sebastian-werner.net>
2010-07-24 21:15:43 +0200

    Tweaked copyright section

--- lib/js/Parser.py
+++ lib/js/Parser.py
@@ -1,48 +1,11 @@
 #
 # JavaScript Tools - Parser Module
-# Copyright 2010 Sebastian Werner
+# License: MPL 1.1/GPL 2.0/LGPL 2.1
+# Authors: 
+#   - Brendan Eich <brendan@mozilla.org> (Original JavaScript) (2004)
+#   - JT Olds <jtolds@xnet5.com> (Python Translation) (2009)
+#   - Sebastian Werner <info@sebastian-werner.net> (Refactoring Python) (2010)
 #
-
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is the Narcissus JavaScript engine, written in Javascript.
-#
-# The Initial Developer of the Original Code is
-# Brendan Eich <brendan@mozilla.org>.
-# Portions created by the Initial Developer are Copyright (C) 2004
-# the Initial Developer. All Rights Reserved.
-#
-# The Python version of the code was created by JT Olds <jtolds@xnet5.com>,
-# and is a direct translation from the Javascript version.
-#
-# This version was refactored by the original Python port by Sebastian 
-# Werner <info@sebastian-werner.net> for a cleaner Python-like implementation
-# with less globals and better structure.
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK ***** */
 
 from js.Node import Node
 from js.Tokenizer import Token
