7fd7221138b9d1318074574fe174b43a4ab41b26
Sebastian Werner <info@sebastian-werner.net>
2010-08-20 19:29:28 +0200

    Added lang module for language specific data

---
+++ lib/js/Lang.py
@@ -0,0 +1,4 @@
+# By Mozilla MDC
+# https://developer.mozilla.org/en/Core_JavaScript_1.5_Guide/Statements#Exception_Types
+EcmaExceptions = ["Error", "EvalError", "RangeError", "ReferenceError", "SyntaxError", "TypeError", "URIError"]
+DomExceptions = ["DOMException", "EventException", "RangeException"]
