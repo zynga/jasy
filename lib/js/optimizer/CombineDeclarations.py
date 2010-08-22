b0739f0e31c6fe8f0884eeb65bf0454bc9d381ed
Sebastian Werner <info@sebastian-werner.net>
2010-08-22 02:38:51 +0200

    Initial working version of declarations combiner

--- lib/js/optimizer/CombineDeclarations.py
+++ lib/js/optimizer/CombineDeclarations.py
@@ -10,9 +10,19 @@
 def optimize(node):
     if not node.type in ("script", "block"):
         return
+
+    # Backwards processing and insertion into previous sibling
+    length = len(node)
+    pos = length-1
+    while pos > 0:
+        child = node[pos]
+        prevChild = node[pos-1]
         
-    for child in node:
-        print child.type
+        if child.type == "var" and prevChild.type == "var":
+            for variable in child:
+                prevChild.append(variable)
+            node.remove(child)
         
+        pos -= 1
     
     
