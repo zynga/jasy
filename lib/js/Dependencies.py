65df968f67c54e404f2b7f1eb8576bb49744346b
Sebastian Werner <info@sebastian-werner.net>
2010-08-25 22:20:16 +0200

    Moved dot/variable operations to js.Util

--- lib/js/Dependencies.py
+++ lib/js/Dependencies.py
@@ -3,7 +3,7 @@
 # Copyright 2010 Sebastian Werner
 #
 
-from copy import copy
+from js.Util import combineVariable
 
 def deps(node):
     names = set()
@@ -36,9 +36,9 @@ def __inspect(node, declared, names, namespaced):
                     names.add(item)
                         
     # Detect namespaced identifiers
-    if node.type == "identifier" and not node.value in declared and getattr(node, "scope", False) and node.parent.type == "dot":
-        name = __combineDot(node)
-        if "." in name:
+    if node.type == "identifier" and not node.value in declared:
+        name = combineVariable(node)
+        if name:
             namespaced.add(name)
 
     # Process children
@@ -46,22 +46,5 @@ def __inspect(node, declared, names, namespaced):
         __inspect(child, declared, names, namespaced)
         
     
-def __combineDot(node):
-    result = node.value
-    
-    # Don't go deeper if name starts with a upper-case
-    # This is normally a hint for a class name.
-    if result[0].upper() == result[0]:
-        return result
-    
-    parent = node.parent
-    if parent.type == "dot":
-        if parent[0] is node:
-            result += "." + __combineDot(parent[1])
-        else:
-            parentParent = parent.parent
-            if parentParent.type == "dot" and parentParent[0] is parent:
-                result += "." + __combineDot(parentParent[1])
-            
-    return result
+
     
--- lib/js/Util.py
+++ lib/js/Util.py
@@ -18,3 +18,34 @@ def baseEncode(num, alphabet=string.letters):
     arr.reverse()
     return "".join(arr)    
     
+    
+def combineVariable(node):
+    """ Combines an identifier node to a namespaced variable. Only returns a (string) value when value is part of a namespaced variable """
+
+    if getattr(node, "scope", False) and node.parent.type == "dot":
+        variable = __combineVariableRecurser(node)
+        if "." in variable:
+            return variable
+
+    return None
+
+
+def __combineVariableRecurser(node):
+    """ Internal helper for namespace builder """
+    result = node.value
+
+    # Don't go deeper if name starts with a upper-case
+    # This is normally a hint for a class name.
+    if result[0].upper() == result[0]:
+        return result
+
+    parent = node.parent
+    if parent.type == "dot":
+        if parent[0] is node:
+            result += "." + __combineVariableRecurser(parent[1])
+        else:
+            parentParent = parent.parent
+            if parentParent.type == "dot" and parentParent[0] is parent:
+                result += "." + __combineVariableRecurser(parentParent[1])
+
+    return result
