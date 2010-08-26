ec75006049300b0be579111ff4c389c335cdd653
Sebastian Werner <info@sebastian-werner.net>
2010-08-26 22:57:36 +0200

    Completed indexer

--- lib/js/Project.py
+++ lib/js/Project.py
@@ -14,43 +14,76 @@ def manifest(folder):
     profile = parser.get("main", "profile")
     namespace = parser.get("main", "namespace")
     
-    
-    
-    globals()["__" + profile](title, folder, namespace)
-    
+    print "Project: %s (%s)" % (title, namespace)
+    classes, resources, translations = globals()["__" + profile](folder, namespace)
+    print "Indexed: %s classes, %s resources, %s translations" % (len(classes), len(resources), len(translations))
 
     return dict(parser.items("main"))
     
     
 
-def __qooxdoo(title, folder, namespace):
-    print "Project: %s (%s)" % (title, namespace)
-    print "Folder: %s" % folder
-    
+def __qooxdoo(folder, namespace):
     classPath = os.path.join(folder, "source", "class", namespace)
     resourcePath = os.path.join(folder, "source", "resource", namespace)
     # TODO: Any chance to move translation into namespace as well?
     translationPath = os.path.join(folder, "source", "translation") 
     
-    
-    print "Class Modification: %s" % os.path.getmtime(classPath)
-    print "Resource Modification:os.path.splitext %s" % os.path.getmtime(resourcePath)
-    print "Translation Modification: %s" % os.path.getmtime(translationPath)
+    # print "Class Modification: %s" % os.path.getmtime(classPath)
+    # print "Resource Modification: %s" % os.path.getmtime(resourcePath)
+    # print "Translation Modification: %s" % os.path.getmtime(translationPath)
 
+    dirFilter = [".svn",".git",".hg"]
 
+    # List classes
+    classes = {}
     classPathLen = len(classPath) + 1
     for dirPath, dirNames, fileNames in os.walk(classPath):
         for dirName in dirNames:
-            if dirName in [".svn",".git",".hg"]:
+            if dirName in dirFilter:
                 dirNames.remove(dirName)
         
         for fileName in fileNames:
-            if "_" in fileName:
+            if fileName[0] == "." or "_" in fileName or not fileName.endswith(".js"):
                 continue
-            
+                
             filePath = os.path.join(dirPath, fileName)
             relPath = filePath[classPathLen:]
             className = os.path.splitext(relPath)[0].replace("/", ".")
             
-            print className
-    
+            classes[className] = filePath
+            
+            
+    # List resources
+    resources = {}
+    resourcePathLen = len(resourcePath) + 1
+    for dirPath, dirNames, fileNames in os.walk(resourcePath):
+        for dirName in dirNames:
+            if dirName in dirFilter:
+                dirNames.remove(dirName)
+        
+        for fileName in fileNames:    
+            if fileName[0] == ".":
+                continue
+            
+            filePath = os.path.join(dirPath, fileName)
+            relPath = filePath[resourcePathLen:]            
+
+            resources[relPath] = filePath
+            
+
+    # List translations    
+    translations = {}
+    for dirPath, dirNames, fileNames in os.walk(translationPath):
+        for dirName in dirNames:
+            if dirName in dirFilter:
+                dirNames.remove(dirName)
+        
+        for fileName in fileNames:    
+            if fileName[0] == "." or not fileName.endswith(".po"):
+                continue
+                
+            translations[os.path.splitext(fileName)[0]] = os.path.join(dirPath, fileName)
+            
+
+
+    return classes, resources, translations
