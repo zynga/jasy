69b089b3e74d59b7d6cb5f2c244f3766f9eb87f5
Sebastian Werner <info@sebastian-werner.net>
2010-09-18 18:42:51 +0200

    Minor cleanup

--- lib/js/__init__.py
+++ lib/js/__init__.py
@@ -2,13 +2,3 @@
 # JavaScript Tools
 # Copyright 2010 Sebastian Werner
 #
-
-import js.Parser
-import js.Dependencies
-import js.Compressor
-
-import js.optimizer.CombineDeclarations
-import js.optimizer.LocalVariables
-import js.optimizer.Variants
-
-from js.Util import compareToKey
