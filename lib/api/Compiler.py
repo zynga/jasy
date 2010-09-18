47054acd0d570a85d30d924cb4bcdad6ef4a43e8
Sebastian Werner <info@sebastian-werner.net>
2010-09-18 19:36:45 +0200

    Worked on compilation

--- lib/api/Compiler.py
+++ lib/api/Compiler.py
@@ -3,14 +3,18 @@
 # Copyright 2010 Sebastian Werner
 #
 
+from js.Compressor import compress
+
 class JsCompiler():
-    def __init__(self, classList):
+    def __init__(self, session, classList):
+        self.session = session
         self.classList = classList
         
     def compile(self):
         result = []
         
         for classObj in self.classList:
+            print("Compile: %s" % classObj)
             tree = classObj.getTree()
             compressed = compress(tree)
             result.append(compressed)
