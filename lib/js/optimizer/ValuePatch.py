6ced17ac67a2ef54034b0d27c4c7bc619e505e93
Sebastian Werner <info@sebastian-werner.net>
2010-10-10 21:09:30 +0200

    Added support for qx.core.Variant.select()

--- lib/js/optimizer/ValuePatch.py
+++ lib/js/optimizer/ValuePatch.py
@@ -24,14 +24,37 @@ def patch(node, permutation):
                 node.parent.replace(node, repl)            
                 modified = True
                 
+            # qooxdoo specific: qx.core.Variant.isSet(value, expected)
             elif assembled == "qx.core.Variant.isSet" and node.parent.type == "call":
-                call = node.parent
-                params = call[1]
+                callNode = node.parent
+                params = callNode[1]
                 replacement = permutation.get(params[0].value)
                 if replacement:
-                    repl = parseExpression("true" if replacement == params[1].value else "false")
-                    call.parent.replace(call, repl)
+                    parsedReplacement = parseExpression(replacement)
+                    if parsedReplacement.value == params[1].value:
+                        replacementNode = parseExpression("true")
+                    else:
+                        replacementNode = parseExpression("false")
+                    
+                    callNode.parent.replace(callNode, replacementNode)
                     modified = True
+    
+            # qooxdoo specific: qx.core.Variant.select(value, map)
+            elif assembled == "qx.core.Variant.select" and node.parent.type == "call":
+                callNode = node.parent
+                params = callNode[1]
+                replacement = permutation.get(params[0].value)
+                if replacement:
+                    parsedReplacement = parseExpression(replacement)
+                    targetIdentifier = parsedReplacement.value
+
+                    # Directly try to find matching identifier in second param (map)
+                    objectInit = params[1]
+                    if objectInit.type == "object_init":
+                        for propertyInit in objectInit:
+                            if propertyInit[0].value == targetIdentifier:
+                                callNode.parent.replace(callNode, propertyInit[1])
+                                break
     
     
     for child in node:
