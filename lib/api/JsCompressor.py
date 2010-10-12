112c40d0172fdbf2bbaa6782bc48b2c7d387d61b
Sebastian Werner <info@sebastian-werner.net>
2010-10-12 18:14:32 +0200

    Minor fix

--- lib/api/JsCompressor.py
+++ lib/api/JsCompressor.py
@@ -19,7 +19,8 @@ class JsCompressor():
         
         logging.info("Compressing classes...")
         result.append("// Permutation: %s" % permutation)
-        result.append("// Hash: %s" % permutation.getHash())
+        if permutation:
+            result.append("// Hash: %s" % permutation.getHash())
         
         for classObj in self.__classList:
             compressed = classObj.getCompressed(permutation)
