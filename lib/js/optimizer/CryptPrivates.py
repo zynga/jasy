cd354208e94a93d79dabb8d10e66e85933842c7f
Sebastian Werner <info@sebastian-werner.net>
2010-10-14 14:43:32 +0200

    Implemented private crypter

--- lib/js/optimizer/CryptPrivates.py
+++ lib/js/optimizer/CryptPrivates.py
@@ -13,9 +13,46 @@ __all__ = ["optimize"]
 # Public API
 #
 
-
-
-def optimize(node, id):
-    print("Crypt privates for: %s" % id)
+def optimize(node, fileId):
+    privates = {}
+    __recurser(node, fileId, privates)
+    return len(privates) > 0
     
+    
+
+#
+# Internal API
+#
+
+def __recurser(node, fileId, privates, modified=False):
+    if node.type == "identifier":
+        value = node.value
+        if type(value) == str and value.startswith("__"):
+            modified = True
+            
+            if value in privates:
+                repl = privates[value]
+            else:
+                repl = "$%s$%s" % (fileId, __baseEncode(len(privates)))
+                privates[value] = repl
+                
+            # Updating identifier
+            node.value = repl
+        
+    for child in node:
+        __recurser(child, fileId, privates)
+    
+    
+    
+def __baseEncode(num, alphabet=string.ascii_letters+string.digits):
+    if (num == 0):
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        rem = num % base
+        num = num // base
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return "".join(arr)
     
