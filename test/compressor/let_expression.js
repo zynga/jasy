88dcc308edf9e2774fd262ff9e19f8b8856753db
Sebastian Werner <info@sebastian-werner.net>
2010-10-16 21:25:54 +0200

    Minor change

--- test/compressor/let_expression.js
+++ test/compressor/let_expression.js
@@ -1,4 +1,4 @@
 var x = 5;  
 var y = 0;  
-document.write( let(x = x + 10, y = 12) x + y + "<br>\n");  
+document.write(let(x = x + 10, y = 12) x + y + "<br>\n");  
 document.write(x+y + "<br>\n");  
