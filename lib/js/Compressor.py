bf9d88b3d44f3b7eac498a1be7ce422d67d05e9f
Sebastian Werner <info@sebastian-werner.net>
2010-10-18 18:58:31 +0200

    Simplified compression of try-catch through removal of custom block compressor logic.

--- lib/js/Compressor.py
+++ lib/js/Compressor.py
@@ -327,17 +327,17 @@ def __throw(node):
     return addSemicolon("throw %s" % compress(node.exception))
 
 def __try(node):
-    result = "try{%s}" % statements(node.tryBlock)
+    result = "try%s" % compress(node.tryBlock)
     
     for catch in node:
         if catch.type == "catch":
             if hasattr(catch, "guard"):
-                result += "catch(%s if %s){%s}" % (compress(catch.exception), compress(catch.guard), statements(catch.block))
+                result += "catch(%s if %s)%s" % (compress(catch.exception), compress(catch.guard), compress(catch.block))
             else:
-                result += "catch(%s){%s}" % (compress(catch.exception), statements(catch.block))
+                result += "catch(%s)%s" % (compress(catch.exception), compress(catch.block))
 
     if hasattr(node, "finallyBlock"):
-        result += "finally{%s}" % statements(node.finallyBlock)
+        result += "finally%s" % compress(node.finallyBlock)
 
     return result
 
