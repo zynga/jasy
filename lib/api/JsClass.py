a22e3480398d3ea22b64ab13dbf63b6ced6df1f2
Sebastian Werner <info@sebastian-werner.net>
2010-10-18 18:41:29 +0200

    Improved naming of new optimizer code. Integrated into project handling.

--- lib/api/JsClass.py
+++ lib/api/JsClass.py
@@ -14,9 +14,10 @@ from js.optimizer.ValuePatch import patch
 from js.optimizer.DeadCode import optimize
 
 # Post optimization
-import js.optimizer.CombineDeclarations as CombineDeclarations
-import js.optimizer.LocalVariables as LocalVariables
-import js.optimizer.CryptPrivates as CryptPrivates
+import js.optimizer.CombineDeclarations
+import js.optimizer.LocalVariables
+import js.optimizer.CryptPrivates
+import js.optimizer.BlockReducer
 
 allIds = {}
 
@@ -90,6 +91,9 @@ class JsClass():
                 
                 if "variables" in optimization:
                     LocalVariables.optimize(tree)
+
+                if "blocks" in optimization:
+                    BlockReducer.optimize(tree)
                     
                 if "declarations" in optimization:
                     CombineDeclarations.optimize(tree)
