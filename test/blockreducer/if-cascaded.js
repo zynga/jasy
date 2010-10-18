f2fb01ba7eca29a3425f53ef775108c116686612
Sebastian Werner <info@sebastian-werner.net>
2010-10-18 19:03:11 +0200

    New testcase

---
+++ test/blockreducer/if-cascaded.js
@@ -0,0 +1,18 @@
+if(something)
+{
+  if (condition)
+  {
+    somethingCase1a;
+    somethingCase1b
+  }
+  else
+  {
+    somethingCase2a;
+    somethingCase2b;
+  }
+}
+else
+{
+  otherStuffA;
+  otherStuffB;
+}
