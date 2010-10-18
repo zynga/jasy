4a952a6fb6d2b5f1ba4e0e0a718ef7b58998c479
Sebastian Werner <info@sebastian-werner.net>
2010-10-18 15:24:58 +0200

    Added return optimization inside if-statements as first easy start.

--- lib/js/optimizer/IfTranslator.py
+++ lib/js/optimizer/IfTranslator.py
@@ -3,14 +3,52 @@
 # Copyright 2010 Sebastian Werner
 #
 
+from js.parser.Node import Node
+import logging
+
 def optimize(node):
     # Process from inside to outside
     for child in node:
-        optimize(node)
-
+        optimize(child)
+        
+    # Unwrap blocks
+    if node.type == "block":
+        if len(node) == 0:
+            print("Replace empty block")
+            node.parent.replace(node, Node(node.tokenizer, "semicolon"))
+        elif len(node) == 1:
+            print("Unwrap block")
+            node.parent.replace(node, node[0])
+            
+    # Process all if-statements
     if node.type == "if":
-        print("Found if")
-        pass
-    
+        thenPart = getattr(node, "thenPart", None)
+        elsePart = getattr(node, "elsePart", None)
+        
+        if thenPart.type == "return" and elsePart.type == "return":
+            ret = createReturn(createHook(node.condition, thenPart.value, elsePart.value))
+            node.parent.replace(node, ret)
+
+
+
+
+
+
+
+
+
+def createReturn(value):
+    ret = Node(value.tokenizer, "return")
+    ret.append(value, "value")
+    return ret
+
+
+def createHook(condition, thenPart, elsePart):
+    hook = Node(condition.tokenizer, "hook")
+    hook.append(condition, "condition")
+    hook.append(thenPart, "thenPart")
+    hook.append(elsePart, "elsePart")
+    return hook
+            
     
     
