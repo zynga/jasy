6b3548b0adfaebb7a8418c25cb63961c97166fd1
Sebastian Werner <info@sebastian-werner.net>
2010-10-23 18:49:23 +0200

    Minor updates

--- test/localvariables/let_statement.js
+++ test/localvariables/let_statement.js
@@ -3,8 +3,8 @@ function wrapper()
   var x = 5;
   var y = 0;
 
-  let (x = x+10, y = 12) {
-    print(x+y + "\n");
+  let (x = x+10, y = 12, z=3) {
+    print(x+y+z + "\n");
   }
 
   print((x + y) + "\n");
