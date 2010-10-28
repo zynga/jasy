fa65c231e2272ad36b35b842f572a011d316c67b
Sebastian Werner <info@sebastian-werner.net>
2010-10-28 18:13:18 +0200

    Reworked var-block handling to reduce major overhead of first version and fix issues in for/for_in loops

--- lib/js/optimizer/UnusedCleaner.py
+++ lib/js/optimizer/UnusedCleaner.py
@@ -44,6 +44,13 @@ def __clean(node, unused):
     """
     
     retval = False
+    
+    # Process children
+    for child in node:
+        if child.type != "function":
+            if __clean(child, unused):
+                retval = True
+                    
 
     if node.type == "script" and hasattr(node, "parent"):
         params = getattr(node.parent, "params", None)
@@ -52,67 +59,80 @@ def __clean(node, unused):
             # as there is not a required one after us.
             for identifier in reversed(params):
                 if identifier.value in unused:
-                    logging.debug("Cleanup '%s' in line %s", identifier.value, identifier.line)
-                    retval = True
+                    # logging.debug("Cleanup '%s' in line %s", identifier.value, identifier.line)
                     params.remove(identifier)
+                    retval = True
                 else:
                     break
+                    
             
-
     elif node.type == "var":
-        node = splitVar(node)
-        
-    elif node.type == "declaration" and node.name in unused:
-        logging.debug("Cleanup '%s' in line %s", node.name, node.line)
-        
-        if hasattr(node, "initializer"):
-            # Replace whole "var" statement with initializer wrapped in semicolon statement
-            # This works because of splitVar splitted statements ealier
-            semicolon = Node(node.tokenizer, "semicolon")
-            semicolon.append(node[0], "expression")
-            node.parent.parent.replace(node.parent, semicolon)
-            node = semicolon
-            retval = True
-        else:
+        later = []
+        for decl in reversed(node):
+            if decl.name in unused:
+                if hasattr(decl, "initializer"):
+                    init = decl.initializer
+                    if init.type in ("null", "this", "true", "false", "identifier", "number", "string", "regexp"):
+                        # Primary initializer => just remove the declaration
+                        node.remove(decl)
+                        retval = True
+                        
+                    else:
+                        later.append(decl)
+                    
+                else:
+                    # No initializer => just remove the declaration
+                    node.remove(decl)
+                    retval = True
+                    
+        # Whether we need to rework the list to remove all declarations cleanly
+        if later:
+            parent = node.parent
+            if parent.type in ("block", "script"):
+                index = parent.index(node)
+                currentVar = node
+                for child in list(node):
+                    if child in later:
+                        # Wrap initializer into semicolon statement
+                        index += 1
+                        semicolon = Node(child.tokenizer, "semicolon")
+                        semicolon.append(child.initializer, "expression")
+                        parent.insert(index, semicolon)
+                        
+                        # Remove original child
+                        node.remove(child)
+                    
+                        # Prepare new var block for following children
+                        index += 1
+                        currentVar = Node(node.tokenizer, "var")
+                        parent.insert(index, currentVar)
+                        
+                    elif child.parent != currentVar:
+                        # Append remaining definitions to current variable block
+                        currentVar.append(child)
+                        
+                # Remove the last created "var" statement if it is unused
+                if len(currentVar) == 0:
+                    currentVar.parent.remove(currentVar)
+                
+            else:
+                # Edge-case: Variable declaration which assigns a complex value, but
+                # where the result is never used. What needs to be happen here is 
+                # that one needs to extract the "var" block to the outside of the loop.
+                # This needs to create a new "block" if there is not yet one, move the "for"
+                # inside it, extract the "var" block to be placed before the "for" 
+                # and then need to rework this new block with the same logic as above.
+                
+                for decl in later:
+                    logging.warn("Unused variables %s at line %s could not be removed automatically." % (decl.name, node.line))
+                
+                
+        elif len(node) == 0:
             node.parent.remove(node)
-            return True
-            
-    # Process children
-    for child in node:
-        if child.type != "function":
-            if __clean(child, unused):
-                retval = True
-            
+
+
+
+
     return retval
-    
-    
-def splitVar(node):
-    """ 
-    Splits one var statement with multiple declarations into a var statement for every declaration.
-    This is mainly for making cleanups easier afterwards and will be optimized later on by
-    the CombineDeclarations optimizer.
-    """
-    
-    # Split:
-    #
-    # var->def1,def2,def3; into
-    # var->def1;var->def2;var->def3;
-    
-    if len(node) > 1:
-        parent = node.parent
-        block = Node(node.tokenizer, "block")
-        
-        helper = None
-        for child in list(node):
-            helper = Node(node.tokenizer, "var")
-            helper.append(child)
-            block.append(helper)
-            
-        parent.replace(node, block)
-        
-        return block
-        
-    return node
-        
-    
+
     
