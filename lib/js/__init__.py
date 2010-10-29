86f17f3e956c8027e31afa641bf8a97cb6124785
Sebastian Werner <info@sebastian-werner.net>
2010-10-29 19:08:51 +0200

    Merged api/* into js/

--- lib/js/__init__.py
+++ lib/js/__init__.py
@@ -2,3 +2,11 @@
 # JavaScript Tools
 # Copyright 2010 Sebastian Werner
 #
+
+from js.Session import *
+from js.Project import *
+from js.Permutation import *
+from js.JsClass import *
+from js.JsResolver import *
+from js.JsSorter import *
+from js.JsCompressor import *
