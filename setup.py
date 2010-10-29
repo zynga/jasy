5b141d5295590a4d0d0d57d35fc2f70a66715fd3
Sebastian Werner <info@sebastian-werner.net>
2010-10-29 16:14:06 +0200

    Initial setup.py for python distutils.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='jstools',
+      version='0.1',
+      author='Sebastian Werner',
+      author_email='info@sebastian-werner.net',
+      url='http://github.com/unify/jstools',
+      packages=['js'],
+      package_dir = {'': 'lib'}
+      )
