3553d388ed98c0555a5d18ebc7a7d0db3b9c8c5a
Sebastian Werner <info@sebastian-werner.net>
2010-10-31 22:08:24 +0100

    Optimized implementation of Permutation in often called methods getKey/getHash

--- lib/js/core/Permutation.py
+++ lib/js/core/Permutation.py
@@ -6,30 +6,13 @@
 import hashlib
 
 class Permutation:
-    def __init__(self, combination, timestamp=None):
+    def __init__(self, combination, timestamp=None, timed=False):
         self.__combination = combination
         self.__timestamp = timestamp
+        self.__key = self.__buildKey(combination, timed)
+        self.__hash = hashlib.md5(self.getKey().encode("utf-8")).hexdigest()
         
-        
-    def has(self, variant):
-        return variant in self.__combination
-        
-        
-    def get(self, variant):
-        if variant in self.__combination:
-            return self.__combination[variant]
-            
-        return None
-        
-        
-    def getKey(self, timed=False):
-        """
-        Returns a key which can be used to identifier the permutation.
-        This key is read and parsable to restore the set of permutations.
-        Might be used for transfering the permutation for string-only communications etc.
-        """
-
-        combination = self.__combination
+    def __buildKey(self, combination, timed=False):
         result = []
         for key in sorted(combination):
             result.append("%s:%s" % (key, combination[key]))
@@ -38,18 +21,22 @@ class Permutation:
             result.append("time:%s" % self.__timestamp)
 
         return "; ".join(result)
-
-    def getHash(self, timed=False):
-        """
-        Returns a 32 character long identifier for the permutation.
-        May optionally be timed aka includes the build runtime which
-        might be useful for correct invalidation (server caching)
-        """
-
-        key = self.getKey(timed)
-        return hashlib.md5(key.encode("utf-8")).hexdigest()
+            
+    def has(self, variant):
+        return variant in self.__combination
         
+    def get(self, variant):
+        if variant in self.__combination:
+            return self.__combination[variant]
+            
+        return None
+        
+    def getKey(self):
+        return self.__key
 
+    def getHash(self):
+        return self.__hash
+        
     # Map Python built-ins
     __repr__ = getKey
     __str__ = getKey
