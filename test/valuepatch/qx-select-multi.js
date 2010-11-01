8eb81397e1cbba54610001fd7700090990a9debf
Sebastian Werner <info@sebastian-werner.net>
2010-11-01 13:55:15 +0100

    Webkit instead of Mshtml to work with current jsoptimize

--- test/valuepatch/qx-select-multi.js
+++ test/valuepatch/qx-select-multi.js
@@ -1,4 +1,4 @@
 clientSpecific = qx.core.Variant.select("qx.client", {
-  "gecko|mshtml" : function() { geckoOrMshtml(); },
+  "gecko|webkit" : function() { geckoOrWebkit(); },
   "default" : function() { other(); }
 });
