00bd1d9d5d3d273e0f13f847b964f83935e7f3f7
Sebastian Werner <info@sebastian-werner.net>
2010-11-13 11:50:51 +0100

    A few more JS-Tools => Jasy renames

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='jstools',
+setup(name='jasy',
       version='0.1',
       author='Sebastian Werner',
       author_email='info@sebastian-werner.net',
       url='http://github.com/unify/jstools',
-      packages=['js'],
+      packages=['jasy'],
       package_dir = {'': 'lib'}
-      )
+)
