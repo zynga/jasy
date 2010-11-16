2d394592bc777d5a582cc68e8f7d540bf6483848
Sebastian Werner <info@sebastian-werner.net>
2010-11-17 00:08:00 +0100

    New tests

--- test/translation/test.js
+++ test/translation/test.js
@@ -6,9 +6,11 @@ function wrapper()
   
   alert(tr("Guten {0}", "Morgen"))
   alert(tr("Guten {0}! {0}!", "Morgen"))
+  alert(tr("Guten {0}! {0}!", this.computeGreeting()))
 
   alert(trc("Chat (noum)", "Chat"));
   alert(trc("Chat (noum) {0}", "Chat {0}", "Online"));
+  alert(trc("Chat (noum) {0}", "Chat {0}", this.getChatStatus()));
 
   alert(trn("You have got a new mail", "You have got new mails", newMails));
   alert(trn("You have got a new mail", "You have got {0} new mails", newMails, newMails));
