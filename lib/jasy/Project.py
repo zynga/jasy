01a530e97868caca757fa67c35c7fc0da095da1d
Sebastian Werner <info@sebastian-werner.net>
2010-11-19 18:04:38 +0100

    Improved manifest handling to support non qooxdoo kind of projects

--- lib/jasy/Project.py
+++ lib/jasy/Project.py
@@ -8,28 +8,43 @@ from configparser import SafeConfigParser
 from jasy.core.Class import Class
 from jasy.core.Cache import Cache
         
+class ProjectException(Exception):
+    pass
+        
 class Project():
     def __init__(self, path):
         if not os.path.isdir(path):
-            raise Exception("Invalid project path: %s (Absolute: %s)" % (path, os.path.abspath(path)))
+            raise ProjectException("Invalid project path: %s (Absolute: %s)" % (path, os.path.abspath(path)))
         
         self.path = path
-        self.dirFilter = [".svn",".git",".hg"]
+        self.dirFilter = [".svn",".git",".hg",".bzr"]
         self.cache = Cache(self.path)
         
         manifestPath = os.path.join(path, "manifest.cfg")
         if not os.path.exists(manifestPath):
-            raise Exception("Invalid manifest configuration: %s" % manifestPath)
+            raise ProjectException("Missing manifest.cfg at: %s" % manifestPath)
         
         parser = SafeConfigParser()
         parser.read(manifestPath)
 
-        self.namespace = parser.get("main", "namespace")
-        logging.info("Initialized project %s" % self.namespace)
+        try:
+            self.name = parser.get("main", "name")
+            self.kind = parser.get("main", "kind")
+        except configparser.NoOptionError:
+            raise ProjectException()
         
-        self.classPath = os.path.join(self.path, "source", "class")
-        self.resourcePath = os.path.join(self.path, "source", "resource")
-        self.translationPath = os.path.join(self.path, "source", "translation")
+        logging.info("Initialized project %s (%s)" % (self.name, self.kind))
+
+        if self.kind == "qooxdoo":
+            self.classPath = os.path.join(self.path, "source", "class")
+            self.resourcePath = os.path.join(self.path, "source", "resource")
+            self.translationPath = os.path.join(self.path, "source", "translation")
+        elif self.kind == "basic":
+            self.classPath = os.path.join(self.path, "src")
+            self.resourcePath = None
+            self.translationPath = None
+        else:
+            raise ProjectException("Unsupported kind of project: %s" % self.kind)
         
 
     def __str__(self):
@@ -65,25 +80,27 @@ class Project():
         except AttributeError:
             classPath = self.classPath
             classes = {}
-            classPathLen = len(classPath) + 1
-            for dirPath, dirNames, fileNames in os.walk(classPath):
-                for dirName in dirNames:
-                    if dirName in self.dirFilter:
-                        dirNames.remove(dirName)
+            
+            if classPath and os.path.exists(classPath):
+                classPathLen = len(classPath) + 1
+                for dirPath, dirNames, fileNames in os.walk(classPath):
+                    for dirName in dirNames:
+                        if dirName in self.dirFilter:
+                            dirNames.remove(dirName)
 
-                for fileName in fileNames:
-                    if fileName[0] == "." or "_" in fileName or not fileName.endswith(".js"):
-                        continue
+                    for fileName in fileNames:
+                        if fileName[0] == "." or "_" in fileName or not fileName.endswith(".js"):
+                            continue
 
-                    filePath = os.path.join(dirPath, fileName)
-                    relPath = filePath[classPathLen:]
+                        filePath = os.path.join(dirPath, fileName)
+                        relPath = filePath[classPathLen:]
 
-                    classObj = Class(filePath, relPath, self)
-                    className = classObj.getName()
+                        classObj = Class(filePath, relPath, self)
+                        className = classObj.getName()
 
-                    classes[className] = classObj
+                        classes[className] = classObj
                 
-            logging.info("Project %s contains %s classes", self.namespace, len(classes))
+            logging.info("Project %s contains %s classes", self.name, len(classes))
             self.classes = classes
             return classes
 
@@ -95,22 +112,24 @@ class Project():
         except AttributeError:
             resourcePath = self.resourcePath
             resources = {}
-            resourcePathLen = len(resourcePath) + 1
-            for dirPath, dirNames, fileNames in os.walk(resourcePath):
-                for dirName in dirNames:
-                    if dirName in self.dirFilter:
-                        dirNames.remove(dirName)
 
-                for fileName in fileNames:    
-                    if fileName[0] == ".":
-                        continue
+            if resourcePath and os.path.exists(resourcePath):
+                resourcePathLen = len(resourcePath) + 1
+                for dirPath, dirNames, fileNames in os.walk(resourcePath):
+                    for dirName in dirNames:
+                        if dirName in self.dirFilter:
+                            dirNames.remove(dirName)
 
-                    filePath = os.path.join(dirPath, fileName)
-                    relPath = filePath[resourcePathLen:]            
+                    for fileName in fileNames:    
+                        if fileName[0] == ".":
+                            continue
 
-                    resources[relPath] = filePath
+                        filePath = os.path.join(dirPath, fileName)
+                        relPath = filePath[resourcePathLen:]            
+
+                        resources[relPath] = filePath
                     
-            logging.info("Project %s contains %s resources", self.namespace, len(resources))
+            logging.info("Project %s contains %s resources", self.name, len(resources))
             self.resources = resources
             return resources
 
@@ -122,18 +141,20 @@ class Project():
         except AttributeError:
             translationPath = self.translationPath
             translations = {}
-            for dirPath, dirNames, fileNames in os.walk(translationPath):
-                for dirName in dirNames:
-                    if dirName in self.dirFilter:
-                        dirNames.remove(dirName)
 
-                for fileName in fileNames:    
-                    if fileName[0] == "." or not fileName.endswith(".po"):
-                        continue
+            if translationPath and os.path.exists(translationPath):
+                for dirPath, dirNames, fileNames in os.walk(translationPath):
+                    for dirName in dirNames:
+                        if dirName in self.dirFilter:
+                            dirNames.remove(dirName)
 
-                    translations[os.path.splitext(fileName)[0]] = os.path.join(dirPath, fileName)
+                    for fileName in fileNames:    
+                        if fileName[0] == "." or not fileName.endswith(".po"):
+                            continue
+
+                        translations[os.path.splitext(fileName)[0]] = os.path.join(dirPath, fileName)
             
-            logging.info("Project %s contains %s translations", self.namespace, len(translations))
+            logging.info("Project %s contains %s translations", self.name, len(translations))
             self.translations = translations
             return translations
         
