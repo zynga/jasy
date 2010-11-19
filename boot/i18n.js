17399fdbf288517ef4fcebe4c67709a0d03a8289
Sebastian Werner <info@sebastian-werner.net>
2010-11-19 17:44:08 +0100

    Some API doc

--- boot/i18n.js
+++ boot/i18n.js
@@ -36,18 +36,45 @@
   // Export object
   global.i18n = 
   {
+    /**
+     * Translates the given message and replaces placeholders in the result string.
+     *
+     * @param msg {String} Message to translate (used as a fallback when no translation is available)
+     * @param varargs {...} Placeholder values
+     * @return {String} Translated string
+     */
     tr : function(msg, varargs)
     {
       var replacement = lookup(msg, msg);
       return varargs == NULL ? replacement : patch(replacement, arguments, 1);
     },
     
+
+    /**
+     * Translates the given message (with a hint for the translator) and 
+     * replaces placeholders in the result string.
+     *
+     * @param hint {String} Lookup name in translation table (including a hint for the translator)
+     * @param msg {String} Fallback message if no translation is available
+     * @param varargs {...} Placeholder values
+     * @return {String} Translated string
+     */
     trc : function(hint, msg, varargs)
     {
       var replacement = lookup(hint, msg)
       return varargs == NULL ? replacement : patch(replacement, arguments, 2);
     },
     
+
+    /**
+     * Translates the given message and replaces placeholders in the result string.
+     *
+     * @param msgSingular {String} Fallback message for singular case
+     * @param msgPlural {String} Fallback message for plural case
+     * @param number {Integer} Number of items (chooses between singular and plural wording)
+     * @param varargs {...} Placeholder values
+     * @return {String} Translated string
+     */
     trn : function(msgSingular, msgPlural, number, varargs)
     {
       var msg = number == 1 ? msgSingular : msgPlural;
