3879a6010906a164a5bc68e1e98f46e56eb019de
Sebastian Werner <info@sebastian-werner.net>
2010-11-19 16:52:19 +0100

    Added new boot sub-project for being used by all projects relying on project handling of Jasy.

---
+++ boot/i18n.js
@@ -0,0 +1,24 @@
+(function()
+{
+  
+  
+  
+  
+  this.i18n = 
+  {
+    tr : function(msg, varargs)
+    {
+      
+    },
+    
+    trc : function(hint, msg, varargs)
+    {
+      
+    },
+    
+    trn : function(msg, no, varargs)
+    {
+      
+    }
+  }
+})();
