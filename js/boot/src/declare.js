9ca2030eb5f7509e1851ca1e5babaff0fbae3fa1
Sebastian Werner <info@sebastian-werner.net>
2010-11-21 15:33:39 +0100

    Namespace declare method added

---
+++ js/boot/src/declare.js
@@ -0,0 +1,22 @@
+function declare(namespace, object)
+{
+  var splits = namespace.split(".");
+  var current = this;
+  var length = splits.length-1;
+  var i = 0;
+  var test;
+  
+  // Fast-check for existing segments
+  while(test=current[splits[i]]) {
+    current = test;
+    i++;
+  }
+
+  // Create missing segments
+  while(i<length) {
+    current = current[splits[i++]] = {};
+  }
+  
+  // Store Object
+  return current[splits[i]] = object;
+}
