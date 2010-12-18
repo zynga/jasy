60228bac8fc928705a5d51074ae1fe8b5a55661e
Sebastian Werner <info@sebastian-werner.net>
2010-12-18 17:05:07 +0100

    New Core class to contain fundamental features for JS development

---
+++ data/jscore/src/Core.js
@@ -0,0 +1,59 @@
+this.Core = 
+{
+  /**
+   * Executes the given code in global context
+   *
+   * Via:
+   * http://perfectionkills.com/global-eval-what-are-the-options/#feature_testing_based_approach
+   *
+   * @param expression {String} Code to execute
+   * @return {var} Return value of executed code
+   */
+  globalEval : (function(global) 
+  {
+    var isIndirectEvalGlobal = (function() 
+    {
+      eval("var Object=123");
+      
+      try 
+      {
+        // Does `Object` resolve to a local variable, or to a global, built-in `Object`, 
+        // reference to which we passed as a first argument?
+        return global.eval('Object') === global.Object;
+      }
+      catch(err) 
+      {
+        // if indirect eval errors out (as allowed per ES3), then just bail out with `false`
+        return false;
+      }
+    })();
+
+    if (isIndirectEvalGlobal) 
+    {
+      // if indirect eval executes code globally, use it
+      return function(expression) {
+        return global.eval(expression);
+      };
+    }
+    else if (global.execScript != null) 
+    {
+      // if `window.execScript exists`, use it
+      return function(expression) {
+        return global.execScript(expression);
+      };
+    }
+
+    // otherwise, globalEval is `undefined` since nothing is returned
+  })(this),
+  
+  
+  /**
+   * Loads the script at the given URI.
+   *
+   * @param uri {String} URI of script source to load
+   */
+  loadScript : function(uri)
+  {
+    // TODO
+  }
+}
