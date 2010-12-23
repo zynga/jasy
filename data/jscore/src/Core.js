26ab686cc353e9f6d6570f8b5c487d60740241df
Sebastian Werner <info@sebastian-werner.net>
2010-12-23 23:46:56 +0100

    More declare feature to Core.js. Formatting of this class and API docs.

--- data/jscore/src/Core.js
+++ data/jscore/src/Core.js
@@ -1,20 +1,41 @@
-this.Core = 
+/* 
+==================================================================================================
+  Jasy - JavaScript Tooling Refined
+  Copyright 2010 Sebastian Werner
+==================================================================================================
+*/
+
+(function(global)
 {
-  /**
-   * Executes the given code in global context
-   *
-   * Via:
-   * http://perfectionkills.com/global-eval-what-are-the-options/#feature_testing_based_approach
-   *
-   * @param expression {String} Code to execute
-   * @return {var} Return value of executed code
-   */
-  globalEval : (function(global) 
+  var declare = function(namespace, object)
+  {
+    var splits = namespace.split(".");
+    var current = global;
+    var length = splits.length-1;
+    var i = 0;
+    var test;
+
+    // Fast-check for existing segments
+    while(test=current[splits[i]]) {
+      current = test;
+      i++;
+    }
+
+    // Create missing segments
+    while(i<length) {
+      current = current[splits[i++]] = {};
+    }
+
+    // Store Object
+    return current[splits[i]] = object;
+  };
+  
+  var globalEval = (function() 
   {
     var isIndirectEvalGlobal = (function() 
     {
       eval("var Object=123");
-      
+
       try 
       {
         // Does `Object` resolve to a local variable, or to a global, built-in `Object`, 
@@ -44,15 +65,37 @@ this.Core =
     }
 
     // otherwise, globalEval is `undefined` since nothing is returned
-  })(this),
+  })();
   
-  
-  /**
-   * Loads the script at the given URI.
-   *
-   * @param uri {String} URI of script source to load
-   */
-  loadScript : LAB.script
-  
-  
-}
+  // Finally create namespace
+  declare("Core",
+  {
+    /**
+     * Declares the given namespace and stores the given object onto it.
+     *
+     * @param namespace {String} Namespace/Package e.g. foo.bar.baz
+     * @param object {Object} Any object
+     */
+    declare : declare,
+
+
+    /**
+     * Executes the given code in global context
+     *
+     * Via:
+     * http://perfectionkills.com/global-eval-what-are-the-options/#feature_testing_based_approach
+     *
+     * @param expression {String} Code to execute
+     * @return {var} Return value of executed code
+     */
+    globalEval : globalEval,
+
+
+    /**
+     * Loads the script at the given URI.
+     *
+     * @param uri {String} URI of script source to load
+     */
+    loadScript : LAB.script
+  }  
+})(this);
