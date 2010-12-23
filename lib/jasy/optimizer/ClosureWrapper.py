b9be76815d53c2a8ff9b58897a27391dd2d8b2b7
Sebastian Werner <info@sebastian-werner.net>
2010-12-23 22:29:54 +0100

    Added initial list of optimizable globals

---
+++ lib/jasy/optimizer/ClosureWrapper.py
@@ -0,0 +1,42 @@
+#
+# Jasy - JavaScript Tooling Refined
+# Copyright 2010 Sebastian Werner
+#
+
+# Via
+# https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects
+GLOBALS = [
+    "Boolean",
+    "Number",
+    "String",
+    "Array",
+    "Object",
+    "Function",
+    "RegExp",
+    "Date",
+
+    "Error",
+    "EvalError",
+    "RangeError",
+    "ReferenceError",
+    "SyntaxError",
+    "TypeError",
+    "URIError",
+
+    "decodeURI",
+    "decodeURIComponent",
+    "encodeURI",
+    "encodeURIComponent",
+
+    "eval",
+    "isFinite",
+    "isNaN",
+    "parseFloat",
+    "parseInt",
+
+    "Infinity",
+    "Math",
+    "NaN",
+    "undefined"
+]
+
