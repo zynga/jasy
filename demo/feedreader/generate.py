28617a885513061636215c4d98649002b23e787e
Sebastian Werner <info@sebastian-werner.net>
2011-01-05 16:09:53 +0100

    Hard-wired classes

--- demo/feedreader/generate.py
+++ demo/feedreader/generate.py
@@ -40,13 +40,14 @@ def source():
     session.addValue("qx.theme", "qx.theme.Modern")
     session.addValue("qx.version", "1.0")
     
-    
-    
     # Build core loader
     logging.info("Building core loader...")
     resolver = Resolver(session.getProjects())
     resolver.addClassName("Core")
     resolver.addClassName("Permutation")
+    resolver.addClassName("detect.Engine")
+    resolver.addClassName("detect.Locale")
+    resolver.addClassName("detect.Param")
     
     optimization = Optimization(["unused", "privates", "variables", "declarations", "blocks"])
     combinedCode = Combiner(None, None, optimization).compress(Sorter(resolver).getSortedClasses())
