92e97072dcc0ab4eeeeb07fe25be59bd5cabc8ba
Sebastian Werner <info@sebastian-werner.net>
2011-01-05 13:09:45 +0100

    New feature test code

---
+++ data/jscore/src/feature/Locale.js
@@ -0,0 +1,11 @@
+Core.declare("feature.Locale", 
+{
+  VALUE : (function()
+  {
+    var nav = navigator;
+    var input = (nav.userLanguage || nav.language).toLowerCase();
+    var split = input.indexOf("-");
+    
+    return split > 0 ? input.substring(0, split) : input;
+  })()  
+});
---
+++ data/jscore/src/feature/Param.js
@@ -0,0 +1,43 @@
+Core.declare("feature.Param",
+{
+  /**
+   * Returns the value of the given parameter
+   *
+   * @param name {String} Parameter name
+   * @return {String} Parameter value
+   */
+  get : (function() 
+  {
+    var items = location.search.substring(1).split("&");
+    var map = {};
+    var undef;
+    
+    var translate = 
+    {
+      "true" : true,
+      "false" : false,
+      "null" : null
+    };    
+    
+    for (var i=0, l=items.length; i<l; i++) 
+    {
+      var item = items[i];
+      var pos = item.indexOf("=");
+      
+      var name = pos == -1 ? item : item.substring(0, pos);
+      var value = pos == -1 ? true : item.substring(pos+1);
+      
+      if (value in translate) {
+        value = translate[value];
+      } else if ("" + parseFloat(value, 10) == value) {
+        value = parseFloat(value, 10);
+      }
+    
+      map[name] = value;
+    }
+    
+    return function(name) {
+      return name in map ? map[name] : null;
+    }
+  })()
+});
