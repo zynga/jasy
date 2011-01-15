37409148f13b5b817a71625465c8d7396aa45d99
Sebastian Werner <info@sebastian-werner.net>
2011-01-16 00:57:21 +0100

    Minor changes

--- data/jscore/src/jasy/Permutation.js
+++ data/jscore/src/jasy/Permutation.js
@@ -4,16 +4,12 @@
  */
 (function()
 {
-  var checksum = (function()
+  var values = jasy.Permutation.values;
+  var tests = jasy.Permutation.tests;
+  
+  var checksum = values ? (function()
   {
-    var values = jasy.Permutation.values;
-    if (!values) {
-      return "";
-    }
-    
     var key = [];
-    var tests = jasy.Permutation.tests;
-    
     for (var name in values) 
     {
       if (name in tests) 
@@ -38,7 +34,7 @@
     var prefix = adler32 < 0 ? "a" : "b";
 
     return prefix + (adler32 < 0 ? -adler32 : adler32).toString(16);
-  })();
+  })() : "";
   
   Core.declare("jasy.Permutation",
   {
@@ -48,7 +44,7 @@
     /** {Map} Maps keys with tests to the classes which implements the test */
     tests : null,
 
-    /** {Number} Holds the checksum for the current permutation which is autodetected by features or by compiled-in data */
+    /** {Number} Holds the checksum for the current permutation which is auto detected by features or by compiled-in data */
     CHECKSUM : checksum,
 
     loadScripts : function(uris)
