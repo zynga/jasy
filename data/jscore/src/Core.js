235d366942339137f76cbe03adaa3892d5f9a589
Sebastian Werner <info@sebastian-werner.net>
2011-01-22 12:01:25 +0100

    Fixed missing declare()

--- data/jscore/src/Core.js
+++ data/jscore/src/Core.js
@@ -7,6 +7,30 @@
 
 (function(global)
 {
+  var declare = function(namespace, object)
+  {
+    var splits = namespace.split(".");
+    var current = global;
+    var length = splits.length-1;
+    var i = 0;
+    var test;
+  
+    // Fast-check for existing segments
+    while(test=current[splits[i]]) 
+    {
+      current = test;
+      i++;
+    }
+  
+    // Create missing segments
+    while(i<length) {
+      current = current[splits[i++]] = {};
+    }
+  
+    // Store Object
+    return current[splits[i]] = object;
+  };
+  
   declare("Core",
   {
     /**
@@ -15,29 +39,7 @@
      * @param namespace {String} Namespace/Package e.g. foo.bar.baz
      * @param object {Object} Any object
      */
-    declare : function(namespace, object)
-    {
-      var splits = namespace.split(".");
-      var current = global;
-      var length = splits.length-1;
-      var i = 0;
-      var test;
-    
-      // Fast-check for existing segments
-      while(test=current[splits[i]]) 
-      {
-        current = test;
-        i++;
-      }
-    
-      // Create missing segments
-      while(i<length) {
-        current = current[splits[i++]] = {};
-      }
-    
-      // Store Object
-      return current[splits[i]] = object;
-    },
+    declare : declare,
 
 
     /**
