299af3e7be2e7952a2c2c5a1221a552c7d4bd67d
Sebastian Werner <swerner@zynga.com>
2011-07-06 17:38:53 +0200

    Worked on Module implementation

--- data/jscore/src/Module.js
+++ data/jscore/src/Module.js
@@ -5,24 +5,55 @@
 ==================================================================================================
 */
 
-/**
- * Define a module with static methods
- * 
- * @param name {String} Name of Module
- * @param members {Map} Data structure containing the members
- */
-Core.declare("Module", function(name, members) {
-	Core.declare(name, members);
+(function() {
+	var genericToString = function() {
+		return "[Module " + this.name + "]";
+	};
 	
-	var prefix = name + ".";
-	var value;
-	
-	for (var key in members) {
-		value = members[key];
-		
-		// Performance would better using typeof but instanceof is required to exclude RegExps
-		if (value instanceof Function) {
-			value.displayName = prefix + key;
+	/**
+	 * Define a module with static methods
+	 * 
+	 * @param name {String} Name of Module
+	 * @param members {Map} Data structure containing the members
+	 */
+	Core.declare("Module", function(name, members) {
+
+		// Store module in namespace first
+		Core.declare(name, members);
+
+		var prefix = name + ".";
+		var value;
+
+		for (var key in members) {
+			value = members[key];
+
+			// Performance would better using typeof but instanceof is required to exclude RegExps
+			if (value instanceof Function) {
+				value.displayName = prefix + key;
+			}
 		}
+
+		// Add module name, implement toString() and valueOf()
+		if(members.name == null) {
+			members.name = name;
+		}
+
+		if(!members.hasOwnProperty("toString")) {
+			members.toString = genericToString;
+		}
+
+		if(!members.hasOwnProperty("valueOf")) {
+			members.valueOf = genericToString;
+		}
+
+		// Mark as module
+		members.__isModule = true;
+	});
+
+	/**
+	 * Whether the given object is a Model
+	 */
+	Module.isModule = function(module) {
+		return !!(module && typeof module == "object" && module.__isModule);
 	}
-});
+})();
