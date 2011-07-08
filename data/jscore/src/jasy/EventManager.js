8b0ed75704bc24bf12272bf5c5773b19e261a732
Sebastian Werner <swerner@zynga.com>
2011-07-08 16:53:33 +0200

    Added initial event handling

---
+++ data/jscore/src/jasy/EventManager.js
@@ -0,0 +1,22 @@
+(function() {
+	Module("jasy.EventManager", {
+		addListener : function(target, type, callback, context, capture) {
+
+		},
+
+		removeListener : function(target, type, callback, context, capture) {
+
+		},
+
+		cleanupObject : function(target) {
+
+		},
+
+		fireEvent : function(eventObj) {
+			var target = eventObj.getTarget();
+			
+			
+		}
+	});
+})();
+ 
