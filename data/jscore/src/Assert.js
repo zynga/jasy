0046533c5b6bba1992a49f8e1866c485727d2284
Sebastian Werner <swerner@zynga.com>
2011-07-12 22:23:14 -0700

    Worked on better test in Assert. Added missing qunit dom nodes

--- data/jscore/src/Assert.js
+++ data/jscore/src/Assert.js
@@ -32,33 +32,57 @@
 		
 		is : function(value, type) {
 			return types[type](value);
+		},
+		
+		getClassName : function(value) {
+			return stringToClass[toString.call(value)];
 		}
 		
 	});
 	
 	// Alias for better compression
-	var cls = Assert;
+	var Assert = global.Assert;
+	var toString = Object.prototype.toString;
 	
-	cls.add(function(value) { return typeof value == "boolean"; }, "isBoolean", "Expected a boolean!");
-	cls.add(function(value) { return value === true; }, "isTrue", "Expected 'true'!");
-	cls.add(function(value) { return value === false; },"isFalse", "Expected 'false'!");
-
-	cls.add(function(value) { return typeof value == "string"; }, "isString", "Expected a string!");
-	cls.add(function(value) { return typeof value == "string" && value.length > 0; }, "isNonEmptyString", "Expected a non empty string!");
-
-	cls.add(function(value) { return typeof value == "number"; }, "isNumber", "Expected a number!");
-	cls.add(function(value) { return value === 0; }, "isZero", "Expected primitive zero!");
-
-	cls.add(function(value) { return typeof value == "object"; }, "isObject", "Expected an object!");
-	cls.add(function(value) { return value === null; }, "isNull", "Expected 'null'!");
-	cls.add(function(value) { return value !== null; }, "isNotNull", "Expected a non-'null' value!");
-	cls.add(function(value) { return typeof value == "object" && value.constructor === global.Object; }, "isMap", "Expected a map (plain object)!");
-	cls.add(function(value) { return typeof value == "object" && value.constructor === global.Array; }, "isArray", "Expected an array!");
-
-	cls.add(function(value) { return typeof value == "function" && value.constructor === global.Function; }, "isFunction", "Expected a function!");
-	cls.add(function(value) { return typeof value == "function" && value.constructor === global.RepExp; }, "isRegExp", "Expected a regular expression!");
+	// Build mapping list for all native global objects
+	var classToString = {
+		Object : toString.call({}),
+		Array : toString.call([]),
+		String : toString.call(''),
+		Boolean : toString.call(true),
+		Number : toString.call(1),
+		Date : toString.call(new Date),
+		RegExp : toString.call(/x/),
+		Function : toString.call(function(){})
+	};
+	
+	var stringToClass = {};
+	for (var cls in classToString) {
+		stringToClass[classToString[cls]] = cls;
+	}
+	
+	var getClassName = Assert.getClassName;
 	
 	
+	Assert.add(function(value) { return typeof value == "boolean"; }, "isBoolean", "Expected a boolean!");
+	Assert.add(function(value) { return value === true; }, "isTrue", "Expected 'true'!");
+	Assert.add(function(value) { return value === false; },"isFalse", "Expected 'false'!");
+
+	Assert.add(function(value) { return typeof value == "string"; }, "isString", "Expected a string!");
+	Assert.add(function(value) { return typeof value == "string" && value.length > 0; }, "isNonEmptyString", "Expected a non empty string!");
+
+	Assert.add(function(value) { return typeof value == "number"; }, "isNumber", "Expected a number!");
+	Assert.add(function(value) { return value === 0; }, "isZero", "Expected primitive zero!");
+
+	Assert.add(function(value) { return typeof value == "object"; }, "isObject", "Expected an object!");
+	Assert.add(function(value) { return value === null; }, "isNull", "Expected 'null'!");
+	Assert.add(function(value) { return value !== null; }, "isNotNull", "Expected a non-'null' value!");
+
+	Assert.add(function(value) { return getClassName(value) == "Object"; }, "isMap", "Expected a map (plain object)!");
+	Assert.add(function(value) { return getClassName(value) == "Array"; }, "isArray", "Expected an array!");
+	Assert.add(function(value) { return getClassName(value) == "Function"; }, "isFunction", "Expected a function!");
+	Assert.add(function(value) { return getClassName(value) == "RegExp"; }, "isRegExp", "Expected a regular expression!");
+	Assert.add(function(value) { return getClassName(value) == "Date"; }, "isDate", "Expected a date object!");
 
 })(this);
 
