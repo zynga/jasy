31f88da8af27d637f55d48892e279c10fa61f705
Sebastian Werner <swerner@zynga.com>
2011-07-13 21:38:17 -0700

    Merged zero timeout into setImmediate.

--- data/jscore/src/ext/setImmediate.js
+++ data/jscore/src/ext/setImmediate.js
@@ -1,18 +1,62 @@
 /**
- * Emulate setImmediate/clearImmediate using timeouts.
+ * Emulate setImmediate/clearImmediate using postMessage or timeouts.
  * 
  * See also: http://dvcs.w3.org/hg/webperf/raw-file/tip/specs/setImmediate/Overview.html
+ *
+ * Inspired by http://mathiasbynens.be/notes/settimeout-onload and
+ * http://dbaron.org/log/20100309-faster-timeouts
+ *
+ * By L. David Baron <dbaron@dbaron.org>, 2010-03-07, 2010-03-09
+ * Released under the following license:
+ *
+ * Copyright (c) 2010, The Mozilla Foundation
+ * All rights reserved.
  *
  * @name {setImmediate}
  */
 (function(global) {
-	if (!global.setImmediate) {
-		global.setImmediate = function(handler) {
-			return setTimeout(handler, 0);
+	if (global.setImmediate) {
+		return;
+	}
+	
+	if (global.postMessage && global.addEventListener) 
+	{
+		var timeouts = [];
+		var messageName = "zero-timeout";
+		
+		global.addEventListener("message", function(ev) 
+		{
+			if (ev.source == global && ev.data == messageName) 
+			{
+				ev.stopPropagation();
+				if (timeouts.length) {
+					timeouts.shift()();
+				}
+			}
+		}, true);
+		
+		global.setImmediate = function setImmediate(func) 
+		{
+			timeouts.push(func);
+			postMessage(messageName, "*");
+			return func; // use function as timeout handle
+		};
+		
+		global.clearImmediate = function clearImmediate(handle) {
+			var pos = timeouts.lastIndexOf(handle);
+			if (pos != -1) {
+				timeouts.splice(pos, 1);
+			}
+		};
+	}
+	else
+	{
+		global.setImmediate = function setImmediate(func) {
+			return setTimeout(func, 0);
 		};
 
-		global.clearImmediate = function(handler) {
-			return clearTimeout(handler);
+		global.clearImmediate = function clearImmediate(handle) {
+			clearTimeout(handle);
 		};
-	}	
+	}
 })(this);
