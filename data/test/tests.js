49cf1df76acf74c5887442b5f4ee4789cc092355
Sebastian Werner <swerner@zynga.com>
2011-07-14 12:35:18 -0700

    Spaces to tabs

--- data/test/tests.js
+++ data/test/tests.js
@@ -1,200 +1,200 @@
 var global = this;
 $(function() {
-  
-  /*
-  ---------------------------------------------------------------------------
-    EXT
-  ---------------------------------------------------------------------------
-  */
-  
-  module("Ext");
-  
-  asyncTest("setTimeout with arguments", 1, function() {
-    setTimeout(function(arg) {
-      equals(arg, "hello");
-      start();
-    }, 10, "hello");
-  });
+	
+	/*
+	---------------------------------------------------------------------------
+		EXT
+	---------------------------------------------------------------------------
+	*/
+	
+	module("Ext");
+	
+	asyncTest("setTimeout with arguments", 1, function() {
+		setTimeout(function(arg) {
+			equals(arg, "hello");
+			start();
+		}, 10, "hello");
+	});
 
-  asyncTest("setImmediate", 1, function() {
-    setImmediate(function() {
-      ok(true, "always fine");
-      start();
-    });
-  });
+	asyncTest("setImmediate", 1, function() {
+		setImmediate(function() {
+			ok(true, "always fine");
+			start();
+		});
+	});
 
 
 
-  /*
-  ---------------------------------------------------------------------------
-    CORE
-  ---------------------------------------------------------------------------
-  */
-  
-  module("Namespace", {
-    teardown : function() {
-      delete global.foo;
-      delete global.abc;
-    }
-  });
-  
-  test("Creating global", function() {
-    Core.declare('foo', 3);
-    equals(global.foo, 3);
-  });
+	/*
+	---------------------------------------------------------------------------
+		CORE
+	---------------------------------------------------------------------------
+	*/
+	
+	module("Namespace", {
+		teardown : function() {
+			delete global.foo;
+			delete global.abc;
+		}
+	});
+	
+	test("Creating global", function() {
+		Core.declare('foo', 3);
+		equals(global.foo, 3);
+	});
 
-  test("Creating namespace", function() {
-    Core.declare('abc.def', 5);
-    equals(global.abc.def, 5);
-  });
-  
-  
+	test("Creating namespace", function() {
+		Core.declare('abc.def', 5);
+		equals(global.abc.def, 5);
+	});
+	
+	
 
-  /*
-  ---------------------------------------------------------------------------
-    MODULES
-  ---------------------------------------------------------------------------
-  */
-  
-  module("Modules", {
-    teardown : function() {
-      delete global.abc;
-    }
-  });
-  
-  test("Creating empty module", function() {
-    Module("abc.Module1", {});
-    equals(Module.isModule(abc.Module1), true);
-    equals(abc.Module1.moduleName, "abc.Module1");
-    equals(abc.Module1.toString(), "[Module abc.Module1]");
-  });
+	/*
+	---------------------------------------------------------------------------
+		MODULES
+	---------------------------------------------------------------------------
+	*/
+	
+	module("Modules", {
+		teardown : function() {
+			delete global.abc;
+		}
+	});
+	
+	test("Creating empty module", function() {
+		Module("abc.Module1", {});
+		equals(Module.isModule(abc.Module1), true);
+		equals(abc.Module1.moduleName, "abc.Module1");
+		equals(abc.Module1.toString(), "[Module abc.Module1]");
+	});
 
-  test("Module validation", function() {
-    equals(Module.isModule({}), false);
-    equals(Module.isModule(3), false);
-    equals(Module.isModule(null), false);
-    equals(Module.isModule({__isModule:true}), false);
-  });
-  
-  test("Creating method module", function() {
-    Module("abc.Module2", {
-      method1 : function() {},
-      method2 : function() {},
-      method3 : function() {}
-    });
-    equals(Module.isModule(abc.Module2), true);
-    ok(abc.Module2.method1 instanceof Function);
-    ok(abc.Module2.method2 instanceof Function);
-    ok(abc.Module2.method3 instanceof Function);
-    equals(abc.Module2.method1.displayName, "abc.Module2.method1");
-    equals(abc.Module2.method2.displayName, "abc.Module2.method2");
-    equals(abc.Module2.method3.displayName, "abc.Module2.method3");
-  });
-  
-  test("Checking module name", function() {
-    raises(function() {
-      Module("", {});
-    });
-    raises(function() {
-      Module(true, {});
-    });
-    raises(function() {
-      Module(" SpaceVoodoo ", {});
-    });
-    raises(function() {
-      Module("has space", {});
-    });
-    raises(function() {
-      Module("firstLow", {});
-    });
-    raises(function() {
-      Module("two..Dots", {});
-    });
-  });
-  
-  
-  
-  /*
-  ---------------------------------------------------------------------------
-    CLASSES
-  ---------------------------------------------------------------------------
-  */
-  
-  module("Classes", {
-    teardown : function() {
-      delete global.abc;
-      delete global.conflict;
-    }
-  });
-  
-  test("Creating empty class", function() {
-    Class("abc.Class1", {});
-    equals(Class.isClass(abc.Class1), true);
-    equals(abc.Class1.className, "abc.Class1");
-    equals(abc.Class1.toString(), "[Class abc.Class1]");
-  });
-  
-  
-  /**
-   * Two classes which should be mixed into another one define the same member. 
-   * A conflict arises, as both could not be merged into the target class.
-   */
-  test("Conflicting members", function() {
-    Class("conflict.Include1", {
-      members : {
-        foo : function() {}
-      }
-    });
-    Class("conflict.Include2", {
-      members : {
-        foo : function() {}
-      }
-    });
+	test("Module validation", function() {
+		equals(Module.isModule({}), false);
+		equals(Module.isModule(3), false);
+		equals(Module.isModule(null), false);
+		equals(Module.isModule({__isModule:true}), false);
+	});
+	
+	test("Creating method module", function() {
+		Module("abc.Module2", {
+			method1 : function() {},
+			method2 : function() {},
+			method3 : function() {}
+		});
+		equals(Module.isModule(abc.Module2), true);
+		ok(abc.Module2.method1 instanceof Function);
+		ok(abc.Module2.method2 instanceof Function);
+		ok(abc.Module2.method3 instanceof Function);
+		equals(abc.Module2.method1.displayName, "abc.Module2.method1");
+		equals(abc.Module2.method2.displayName, "abc.Module2.method2");
+		equals(abc.Module2.method3.displayName, "abc.Module2.method3");
+	});
+	
+	test("Checking module name", function() {
+		raises(function() {
+			Module("", {});
+		});
+		raises(function() {
+			Module(true, {});
+		});
+		raises(function() {
+			Module(" SpaceVoodoo ", {});
+		});
+		raises(function() {
+			Module("has space", {});
+		});
+		raises(function() {
+			Module("firstLow", {});
+		});
+		raises(function() {
+			Module("two..Dots", {});
+		});
+	});
+	
+	
+	
+	/*
+	---------------------------------------------------------------------------
+		CLASSES
+	---------------------------------------------------------------------------
+	*/
+	
+	module("Classes", {
+		teardown : function() {
+			delete global.abc;
+			delete global.conflict;
+		}
+	});
+	
+	test("Creating empty class", function() {
+		Class("abc.Class1", {});
+		equals(Class.isClass(abc.Class1), true);
+		equals(abc.Class1.className, "abc.Class1");
+		equals(abc.Class1.toString(), "[Class abc.Class1]");
+	});
+	
+	
+	/**
+	 * Two classes which should be mixed into another one define the same member. 
+	 * A conflict arises, as both could not be merged into the target class.
+	 */
+	test("Conflicting members", function() {
+		Class("conflict.Include1", {
+			members : {
+				foo : function() {}
+			}
+		});
+		Class("conflict.Include2", {
+			members : {
+				foo : function() {}
+			}
+		});
 
-    raises(function() {
-      Class("conflict.Join", {
-        include : [conflict.Include1, conflict.Include2]
-      });
-    });
-  });
-  
-  
-  /**
-   * Two classes which should be mixed into another one define the same member. 
-   * The conflict is prevented as the affected member is also defined locally. So
-   * the author of the including class is aware of the conflict and could call the
-   * original methods if that makes sense.
-   */
-  test("Conflicting members with merge", function() {
-    Class("conflict.Include1", {
-      members : {
-        foo : function() {}
-      }
-    });
-    Class("conflict.Include2", {
-      members : {
-        foo : function() {}
-      }
-    });
+		raises(function() {
+			Class("conflict.Join", {
+				include : [conflict.Include1, conflict.Include2]
+			});
+		});
+	});
+	
+	
+	/**
+	 * Two classes which should be mixed into another one define the same member. 
+	 * The conflict is prevented as the affected member is also defined locally. So
+	 * the author of the including class is aware of the conflict and could call the
+	 * original methods if that makes sense.
+	 */
+	test("Conflicting members with merge", function() {
+		Class("conflict.Include1", {
+			members : {
+				foo : function() {}
+			}
+		});
+		Class("conflict.Include2", {
+			members : {
+				foo : function() {}
+			}
+		});
 
-    Class("conflict.Join", {
-      include : [conflict.Include1, conflict.Include2],
-      
-      members : {
-        // Merge conflict manually
-        foo : function() {
-          conflict.Include1.prototype.foo.call(this);
-          conflict.Include2.prototype.foo.call(this);
-          
-          doSomethingElse();
-        }
-      }
-    });
-  });
-  
-  
-  
-  
-  
-  
+		Class("conflict.Join", {
+			include : [conflict.Include1, conflict.Include2],
+			
+			members : {
+				// Merge conflict manually
+				foo : function() {
+					conflict.Include1.prototype.foo.call(this);
+					conflict.Include2.prototype.foo.call(this);
+					
+					doSomethingElse();
+				}
+			}
+		});
+	});
+	
+	
+	
+	
+	
+	
 });
