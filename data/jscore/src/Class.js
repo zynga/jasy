d08091cc22cbb42d98fd26059c9c806db707ef43
Sebastian Werner <swerner@zynga.com>
2011-07-14 12:17:20 -0700

    Cleanups

--- data/jscore/src/Class.js
+++ data/jscore/src/Class.js
@@ -10,37 +10,27 @@
 		return "[Class " + this.className + "]";
 	};
 	
-	var detectConflicts = function(obj1, obj2, where, target) {
-		for (var key in obj1) {
-			if (key in obj2) {
-				throw new Error("Conflicting fields in " + where + " through " + obj1 + " and " + obj2 + " joining in: " + target);
-			}
-		}
-	};
-	
-	var dontCopyMembers = {
-		construct : 1,
-		destruct : 1,
-		reset : 1
-	};
-	
 	var isClassValue = +new Date;
+
 	
 	Core.declare("Class", function(name, config) {
 	
 		if (Permutation.isSet("debug")) {
 			Assert.assertModuleName(name, "Invalid class name!");
 			Assert.assertMap(config, "Invalid class configuration in class " + name);
+			if (config.construct) {
+				Assert.assertFunction("Invalid class constructor in: " + name + "!");
+			}
 		}
-
-		if (Permutation.isSet("debug")) {
-			config.construct && Assert.assertFunction("Invalid class constructor in: " + name + "!");
-		}
+		
 		var construct = config.construct || function construct(){};
 	
 		// Store name / type
 		construct.className = name;
 		construct.__isClass = isClassValue;
+	
+		// Add displayName
+		construct.displayName = name;
 	
 		// Add toString() / valueOf()
 		construct.toString = genericToString;
@@ -71,9 +61,6 @@
 						
 						for(var j=0, jl=mixinMemberKeys.length; j<jl; j++) {
 							var mixinMemberKey = mixinMemberKeys[j];
-							if (dontCopyMembers[mixinMemberKey]) {
-								continue;
-							}
 							if (includeMemberKeys.hasOwnProperty(mixinMemberKey)) {
 								throw new Error('Conflicting member "' + mixinMemberKey + '" between classes ' + includeMemberKeys[mixinMemberKey] + ' and ' + mixin);
 							}
@@ -107,24 +94,18 @@
 				entry = proto[key] = members[key];
 
 				if (entry instanceof Function) {
+					entry.displayName = name + "." + key;
 
 					if (orig instanceof Function) {
 						entry.__super = orig;
 					}
-
-					entry.displayName = name + "." + key;
 				}
 			}
 		}
 		
 		
 
-		
-		// Add destructor 
-		proto.destruct = config.destruct || function destruct(){};
 	
-		// Add reset method
-		proto.reset = config.reset || function reset(){};
 	
 		// Add properties
 		var properties = construct.__properties = config.properties || {};
@@ -191,6 +172,6 @@
 	
 	// Add assertions
 	Assert.add(isClass, "isClass", "Invalid class!");
-	Assert.add(includesClass, "includesClass", "Does not include mixin %1!");
+	Assert.add(includesClass, "includesClass", "Does not include class %1!");
 	
 })();
