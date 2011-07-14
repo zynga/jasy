4ed409d41a2f6206a742804cbca78745f616a07b
Sebastian Werner <swerner@zynga.com>
2011-07-13 23:37:59 -0700

    Added some missing features for ES5 shimming

--- data/jscore/src/ext/Object.js
+++ data/jscore/src/ext/Object.js
@@ -5,6 +5,10 @@
 ==================================================================================================
 */
 
+// Some code is based on the work of:
+// ES5 Shim
+// MIT License, Copyright (c) 2009, 280 North Inc. http://280north.com/ 
+
 if (!Object.isObject) {
 	/**
 	 * Whether the given value is an object.
@@ -32,3 +36,51 @@ if (!Object.isObject) {
 	}
 })(Object.prototype.toString);
 
+
+// ES5 15.2.3.14
+// http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation
+if (!Object.keys) {
+
+    var hasDontEnumBug = true,
+        dontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    for (var key in {"toString": null})
+        hasDontEnumBug = false;
+
+    Object.keys = function keys(object) {
+
+        if (
+            typeof object !== "object" && typeof object !== "function"
+            || object === null
+        )
+            throw new TypeError("Object.keys called on a non-object");
+
+        var keys = [];
+        for (var name in object) {
+            if (owns(object, name)) {
+                keys.push(name);
+            }
+        }
+
+        if (hasDontEnumBug) {
+            for (var i = 0, ii = dontEnumsLength; i < ii; i++) {
+                var dontEnum = dontEnums[i];
+                if (owns(object, dontEnum)) {
+                    keys.push(dontEnum);
+                }
+            }
+        }
+
+        return keys;
+    };
+
+}
--- data/jscore/src/ext/String.js
+++ data/jscore/src/ext/String.js
@@ -5,11 +5,25 @@
 ==================================================================================================
 */
 
+// Some code is based on the work of:
+// ES5 Shim
+// MIT License, Copyright (c) 2009, 280 North Inc. http://280north.com/ 
+
 if (!String.isString) 
 {
 	String.isString = function(arg) {
 		return typeof arg == "string" || Object.prototype.toString.call(arg) === "[object String]";
 	};
+}
+
+// ES5 15.5.4.20
+if (!String.prototype.trim) {
+    // http://blog.stevenlevithan.com/archives/faster-trim-javascript
+    var trimBeginRegexp = /^\s\s*/;
+    var trimEndRegexp = /\s\s*$/;
+    String.prototype.trim = function trim() {
+        return String(this).replace(trimBeginRegexp, '').replace(trimEndRegexp, '');
+    };
 }
 
 if (!String.prototype.contains) 
