a54f09b72399887ecd0a6599c3229e7861b09ba1
Sebastian Werner <swerner@zynga.com>
2011-07-18 19:57:15 -0700

    Minor fix

--- data/jscore/src/es5/String.js
+++ data/jscore/src/es5/String.js
@@ -8,11 +8,14 @@
 // ES5 15.5.4.20
 if (!String.prototype.trim) 
 {
-	// http://blog.stevenlevithan.com/archives/faster-trim-javascript
-	var trimBeginRegexp = /^\s\s*/;
-	var trimEndRegexp = /\s\s*$/;
-	
-	String.prototype.trim = function trim() {
-		return String(this).replace(trimBeginRegexp, '').replace(trimEndRegexp, '');
-	};
+	(function() 
+	{
+		// http://blog.stevenlevithan.com/archives/faster-trim-javascript
+		var trimBeginRegexp = /^\s\s*/;
+		var trimEndRegexp = /\s\s*$/;
+
+		String.prototype.trim = function trim() {
+			return String(this).replace(trimBeginRegexp, '').replace(trimEndRegexp, '');
+		};
+	})();
 }
