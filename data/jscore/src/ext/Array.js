fea76a42115af1f133c33df70b4a62bac35fe7c8
Sebastian Werner <info@sebastian-werner.net>
2011-07-27 15:13:05 +0200

    Tweaked implementation to use new Object methods.

--- data/jscore/src/ext/Array.js
+++ data/jscore/src/ext/Array.js
@@ -5,45 +5,58 @@
 ==================================================================================================
 */
 
-/**
- * Converts the given arguments object into an array.
- *
- * Via: http://jsperf.com/arrayifying-arguments/4
- *
- * @param args {arguments} Arguments object to convert
- * @return {Array} Created array instance
- */
-Array.fromArguments = function fromArguments(args) {
-	return Array.apply(null, args);
-};
+Object.addObjectMethods("Array", 
+{
+	/**
+	 * Converts the given arguments object into an array.
+	 *
+	 * Via: http://jsperf.com/arrayifying-arguments/4
+	 *
+	 * @param args {arguments} Arguments object to convert
+	 * @return {Array} Created array instance
+	 */
+	fromArguments : function(args) {
+		return Array.apply(null, args);
+	}
+});
 
+Object.addPrototypeMethods("Array", 
+{
+	/**
+	 * Returns the maximum number in the array.
+	 *
+	 * @return {Number} Maximum number
+	 */
+	max : function() {
+		return Math.max.apply(Math, this);
+	},
 
-/**
- * Returns the maximum number in the array.
- *
- * @return {Number} Maximum number
- */
-Array.prototype.max = function max(){
-	return Math.max.apply(Math, this);
-};
+	/**
+	 * Returns the minimum number in the array.
+	 *
+	 * @return {Number} Minimum number
+	 */
+	min : function() {
+		return Math.min.apply(Math, this);
+	},
 
+	/**
+	 * Whether the array contains the given value
+	 *
+	 * @param value {var} Any value
+	 * @return {Boolean} Whether the value was found in the array
+	 */
+	contains : function(value) {
+		return ~this.indexOf(value);
+	},
 
-/**
- * Returns the minimum number in the array.
- *
- * @return {Number} Minimum number
- */
-Array.prototype.min = function min(){
-	return Math.min.apply(Math, this);
-};
+	// Array Remove - By John Resig (MIT Licensed)
+	// http://ejohn.org/blog/javascript-array-remove/
+	removeRange : function(from, to) 
+	{
+		var rest = this.slice((to || from) + 1 || this.length);
+		this.length = from < 0 ? this.length + from : from;
+		return this.push.apply(this, rest);
+	}
+})
 
-
-/**
- * Whether the array contains the given value
- *
- * @param value {var} Any value
- * @return {Boolean} Whether the value was found in the array
- */
-Array.prototype.contains = function contains(value) {
-	return ~this.indexOf(value);
-};
--- data/jscore/src/ext/Function.js
+++ data/jscore/src/ext/Function.js
@@ -5,44 +5,47 @@
 ==================================================================================================
 */
 
-/**
- * Debounces the given method.
- *
- * Debouncing ensures that exactly one signal is sent for an event that may be happening 
- * several times — or even several hundreds of times over an extended period. As long as 
- * the events are occurring fast enough to happen at least once in every detection 
- * period, the signal will not be sent!
- *
- * Via: http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
- *
- * @param threshold {Integer} Number of milliseconds of distance required before reacting/resetting.
- * @param execAsap {Boolean?false} Whether the execution should happen at begin.
- * @return {Function} Debounced method
- */
-Function.prototype.debounce = function debounce(threshold, execAsap) 
+Object.addPrototypeMethods(Function, "Function", 
 {
-	var func = this;
-	var timeout;
- 
-	return function debounced() 
+	/**
+	 * Debounces the given method.
+	 *
+	 * Debouncing ensures that exactly one signal is sent for an event that may be happening 
+	 * several times — or even several hundreds of times over an extended period. As long as 
+	 * the events are occurring fast enough to happen at least once in every detection 
+	 * period, the signal will not be sent!
+	 *
+	 * Via: http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+	 *
+	 * @param threshold {Integer} Number of milliseconds of distance required before reacting/resetting.
+	 * @param execAsap {Boolean?false} Whether the execution should happen at begin.
+	 * @return {Function} Debounced method
+	 */
+	debounce : function(threshold, execAsap) 
 	{
-		var obj = this, args = arguments;
-		function delayed() 
+		var func = this;
+		var timeout;
+ 
+		return function debounced() 
 		{
-			if (!execAsap) {
+			var obj = this, args = arguments;
+			function delayed() 
+			{
+				if (!execAsap) {
+					func.apply(obj, args);
+				}
+			
+				timeout = null; 
+			};
+ 
+			if (timeout){
+				clearTimeout(timeout); 
+			} else if (execAsap) {
 				func.apply(obj, args);
 			}
-			
-			timeout = null; 
+ 
+			timeout = setTimeout(delayed, threshold || 100); 
 		};
- 
-		if (timeout){
-			clearTimeout(timeout); 
-		} else if (execAsap) {
-			func.apply(obj, args);
-		}
- 
-		timeout = setTimeout(delayed, threshold || 100); 
-	};
-};
+	}
+});
 
