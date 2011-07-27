8857c12e77140a79f92e8abd49e0cce0bb4b7b06
Sebastian Werner <info@sebastian-werner.net>
2011-07-27 15:12:13 +0200

    Added new methods to more safely extend methods.

---
+++ data/jscore/src/ext/Object.js
@@ -0,0 +1,60 @@
+(function(global) {
+	
+	// defineProperty exists in IE8 but will error when trying to define a property on
+	// native objects. IE8 does not have defineProperies, however, so this check saves a try/catch block.
+	if(Object.defineProperty && Object.defineProperties)
+	{
+		var addMember = function(target, name, method) {
+			Object.defineProperty(target, name, { value: method, configurable: true, enumerable: false, writeable: true });
+		};
+	}
+	else 
+	{
+		var addMember = function(target, name, method) {
+			target[name] = method;
+		};
+	};
+
+	var addObjectMethods = function(globalName, members) 
+	{
+		var prefix = globalName + ".";
+		var construct = global[globalName];
+
+		for (var name in members) 
+		{
+			var func = members[name];
+			func.displayName = prefix + name;
+
+			addMember(construct, name, func);
+		}
+	};
+
+	var addPrototypeMethods = function(globalName, members) 
+	{
+		var prefix = globalName + ".prototype.";
+		var proto = global[globalName].prototype;
+
+		for (var name in members) 
+		{
+			var func = members[name];
+			func.displayName = prefix + name;
+
+			addMember(proto, name, func);
+		}
+	};
+	
+	addObjectMethods("Object", 
+	{
+		/**
+		 * 
+		 */
+		addObjectMethods : addObjectMethods,
+
+		/**
+		 * 
+		 */
+		addPrototypeMethods : addPrototypeMethods
+	});
+	
+})(this);
+
