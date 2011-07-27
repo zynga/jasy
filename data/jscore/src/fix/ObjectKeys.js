375b5855d680dbc845a7776d54e2638a84f0cfb6
Sebastian Werner <info@sebastian-werner.net>
2011-07-27 15:35:27 +0200

    Added empty() method. Improved logic and fixed issue with undefined var 'owns'

--- data/jscore/src/fix/ObjectKeys.js
+++ data/jscore/src/fix/ObjectKeys.js
@@ -5,45 +5,75 @@
 ==================================================================================================
 */
 
-if (!Object.keys) 
+(function(Object) 
 {
 	// Fix for IE bug with enumerables
 	var hasDontEnumBug = true;
-	var dontEnums = "toString,toLocaleString,valueOf,hasOwnProperty,isPrototypeOf,propertyIsEnumerable,constructor".split(",");
-
-	var dontEnumsLength = dontEnums.length;
 	for (var key in {"toString": null}) {
 		hasDontEnumBug = false;	
 	}
-
-	// ES5 15.2.3.14
-	// Based on the work of ES5 Shim
-	// MIT License, Copyright (c) 2009, 280 North Inc. http://280north.com/		
-	// http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation
-	Object.keys = function keys(object) 
+	
+	if (hasDontEnumBug) 
 	{
-		if (typeof object !== "object" && typeof object !== "function" || object === null) {
-			throw new TypeError("Object.keys called on a non-object");
-		}
+		var dontEnums = "toString,toLocaleString,valueOf,hasOwnProperty,isPrototypeOf,propertyIsEnumerable,constructor".split(",");
+		var dontEnumsLength = dontEnums.length;
+	}
+	
+	var owns = Object.hasOwnProperty;
 
-		var keys = [];
-		for (var name in object) {
-			if (owns(object, name)) {
-				keys.push(name);
-			}
-		}
-
-		if (hasDontEnumBug) 
+	if (!Object.keys) 
+	{
+		// ES5 15.2.3.14
+		// Based on the work of ES5 Shim
+		// MIT License, Copyright (c) 2009, 280 North Inc. http://280north.com/		
+		// http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation
+		Object.keys = function keys(object) 
 		{
-			for (var i = 0, ii = dontEnumsLength; i < ii; i++) 
-			{
-				var dontEnum = dontEnums[i];
-				if (owns(object, dontEnum)) {
-					keys.push(dontEnum);
+			Assert.assertObjectOrFunction(object);
+
+			var keys = [];
+			for (var name in object) {
+				if (owns(object, name)) {
+					keys.push(name);
 				}
 			}
-		}
 
-		return keys;
-	};
-}
+			if (hasDontEnumBug) 
+			{
+				for (var i=0; i<dontEnumsLength; i++) 
+				{
+					var dontEnum = dontEnums[i];
+					if (owns(object, dontEnum)) {
+						keys.push(dontEnum);
+					}
+				}
+			}
+
+			return keys;
+		};
+	}
+
+	if (!Object.isEmpty)
+	{
+		Object.empty = function(object) 
+		{
+			Assert.assertObjectOrFunction(object);
+			
+			for (var name in object) {
+				return false;
+			}
+
+			if (hasDontEnumBug) 
+			{
+				for (var i=0; i<dontEnumsLength; i++) 
+				{
+					if (owns(object, dontEnums[i])) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		};
+	}
+})(Object);
