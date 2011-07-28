1a7e23a6bf899be5aa7e448eff68a7159d3ec772
Sebastian Werner <info@sebastian-werner.net>
2011-07-29 00:13:27 +0200

    Tweaked code

--- data/jscore/src/fix/setTimeout.js
+++ data/jscore/src/fix/setTimeout.js
@@ -2,38 +2,43 @@
 ==================================================================================================
   Jasy - JavaScript Tooling Refined
   Copyright 2010-2011 Sebastian Werner
+--------------------------------------------------------------------------------------------------
+  Based on the work of Andrea Giammarchi
+  Vice-Versa Project: http://code.google.com/p/vice-versa/
+  http://webreflection.blogspot.com/2011/05/settimeout-and-setinterval-with-extra.html
+  MIT LICENSE
 ==================================================================================================
 */
 
-// Adds support for extra parameters for setInterval/setTimeout for browsers missing it
-// Via: http://webreflection.blogspot.com/2011/05/settimeout-and-setinterval-with-extra.html
-// Only missing in IE browsers
-setTimeout(function(one) {
-	// only if not supported ...
-	if (one) {
+/**
+ * Adds support for extra parameters for setInterval/setTimeout for browsers (IE, ...) missing it
+ */
+(function(global, slice, undef) 
+{
+	// Only fix where support is missing
+	if (this.setTimeout.length !== undef) {
 		return;
 	}
-
-	var slice = Array.prototype.slice;
-
+	
 	// trap original versions
-	var Timeout = setTimeout;
-	var Interval = setInterval;
+	var origTimeout = global.setTimeout;
+	var origInterval = global.setInterval;
 	
 	// create a delegate
-	var delegate = function(callback, args) {
+	var delegate = function(callback, args) 
+	{
 		args = slice.call(args, 2);
 		return function() {
 			callback.apply(null, args);
 		};
 	};
 	
-	// redefine original versions
-	setTimeout = function(callback, delay) {
-		return Timeout(delegate(callback, arguments), delay);
+	// redefine original implementation
+	global.setTimeout = function(callback, delay) {
+		return origTimeout(delegate(callback, arguments), delay);
 	};
 	
-	setInterval = function(callback, delay) {
-		return Interval(delegate(callback, arguments), delay);
+	global.setInterval = function(callback, delay) {
+		return origInterval(delegate(callback, arguments), delay);
 	};
-}, 0, 1);
+})(this, Array.prototype.slice);
