173d2ce63462c6d33e45bce4667d13375582f699
Sebastian Werner <info@sebastian-werner.net>
2011-07-30 15:44:34 +0200

    New version detector database

---
+++ data/jscore/src/jasy/detect/Version.js
@@ -0,0 +1,36 @@
+/* 
+==================================================================================================
+  Jasy - JavaScript Tooling Refined
+  Copyright 2010-2011 Sebastian Werner
+==================================================================================================
+*/
+
+/**
+ * Version database which maps engine versions to released software versions.
+ * 
+ * Can be used to verify whether a software version makes sense or to relate
+ * unknown products (and versions) to known software.
+ *
+ * Gecko 5.0 => Firefox 5.0
+ * Gecko 2.0 => Firefox 4.0
+ * Gecko 1.9.2 = Firefox 3.6
+ * Gecko 1.9.1 => Firefox 3.5
+ * Gecko 1.9 => Firefox 3.0
+ * 
+ * Webkit 528.8 => Chrome 1.0/Safari 4.0
+ * Webkit 533.8 => Chrome 6.0/Safari 5.0/Safari 4.1
+ * Webkit 534.7 => Chrome 7.0
+ * Webkit 535 => Safari 6.0/Chrome 13.0
+ * 
+ * Trident 6.0 => IE10
+ * Trident 5.0 => IE9
+ * Trident 4.0 => IE8
+ * Trident 3.1 => IE7 Mobile
+ * Before = IE7 oder IE6
+ * 
+ * Presto 
+ *
+ */
+(function(global) {
+	
+})(this);
