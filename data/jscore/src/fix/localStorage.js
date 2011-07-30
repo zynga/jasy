cd279a2994f587bc6cf07c25ed5d0b73dbb6ff17
Sebastian Werner <info@sebastian-werner.net>
2011-07-30 15:41:51 +0200

    Added new preliminary polyfill files (still miss implementation)

---
+++ data/jscore/src/fix/localStorage.js
@@ -0,0 +1,20 @@
+/* 
+==================================================================================================
+  Jasy - JavaScript Tooling Refined
+  Copyright 2010-2011 Sebastian Werner
+--------------------------------------------------------------------------------------------------
+==================================================================================================
+*/
+
+(function(global) {
+	
+	// Native support with IE 8, Firefox 3.5, Safari 4.0, Chrome 9.0, Opera 10.6, iOS 3.2, Android 2.1
+	// http://caniuse.com/#feat=namevalue-storage
+	if (global.localStorage) {
+		return;
+	}
+	
+	// - Emulate localStorage using userData behavior (see: lawnchair)
+	// - Emulate sessionStorage using window.name + rc4 crypt (+ cookies) (see: webreflection sessionstorage)
+	
+})(this);
---
+++ data/jscore/src/fix/matchesSelector.js
@@ -0,0 +1,3 @@
+// http://caniuse.com/matchesselector
+// Available with IE9, Firefox 3.6, Safari 5.0, Chrome 4.0, Opera 11.5, iOS 4.0, Android 2.2
+// TODO: Emulate and fix vendor prefixes (like in requestAnimationFrame)
