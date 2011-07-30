a461724c02a372e92bdc11d89a4af989bb1f15ef
Sebastian Werner <info@sebastian-werner.net>
2011-07-30 15:53:56 +0200

    Improved sort order. Added presto versions. Added Firefox 6

--- data/jscore/src/jasy/detect/Version.js
+++ data/jscore/src/jasy/detect/Version.js
@@ -11,25 +11,35 @@
  * Can be used to verify whether a software version makes sense or to relate
  * unknown products (and versions) to known software.
  *
- * Gecko 5.0 => Firefox 5.0
- * Gecko 2.0 => Firefox 4.0
- * Gecko 1.9.2 = Firefox 3.6
- * Gecko 1.9.1 => Firefox 3.5
  * Gecko 1.9 => Firefox 3.0
+ * Gecko 1.9.1 => Firefox 3.5
+ * Gecko 1.9.2 = Firefox 3.6
+ * Gecko 2.0 => Firefox 4.0
+ * Gecko 5.0 => Firefox 5.0
+ * Gecko 6.0 => Firefox 6.0
  * 
  * Webkit 528.8 => Chrome 1.0/Safari 4.0
  * Webkit 533.8 => Chrome 6.0/Safari 5.0/Safari 4.1
  * Webkit 534.7 => Chrome 7.0
- * Webkit 535 => Safari 6.0/Chrome 13.0
+ * Webkit 534.30 => Chrome 12.0
+ * Webkit 534.48 => Safari 5.1
+ * Webkit 535.1 => Chrome 14.0
  * 
- * Trident 6.0 => IE10
- * Trident 5.0 => IE9
- * Trident 4.0 => IE8
- * Trident 3.1 => IE7 Mobile
  * Before = IE7 oder IE6
+ * Trident 3.1 => IE7 Mobile
+ * Trident 4.0 => IE8
+ * Trident 5.0 => IE9
+ * Trident 6.0 => IE10
  * 
- * Presto 
- *
+ * Presto 2.1 => Opera 9.5
+ * Presto 2.2 => Opera 9.7
+ * Presto 2.2.15 => Opera 10 / Mobile 9.8
+ * Presto 2.4 => Mobile 10.0
+ * Presto 2.5 => Opera 10.5 / Mobile 10.1
+ * Presto 2.6 => Opera 10.6
+ * Presto 2.7 => Opera 11.0 / Mobile 11.0
+ * Presto 2.8 => Opera 11.1 / Mobile 11.1
+ * Presto 2.9 => Opera 11.5
  */
 (function(global) {
 	
