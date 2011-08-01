8468e77f1c823c56430f868db2b46a51ecd0ddc2
Sebastian Werner <info@sebastian-werner.net>
2011-08-01 21:58:25 +0200

    Generic set/get

---
+++ data/jscore/src/jasy/property/MGeneric.js
@@ -0,0 +1,28 @@
+/* 
+==================================================================================================
+  Jasy - JavaScript Tooling Refined
+  Copyright 2010-2011 Sebastian Werner
+==================================================================================================
+*/
+
+Class("jasy.property.MGeneric",
+{
+	members : 
+	{
+		set : function(property, value) {
+			if (arguments.length == 2) {
+				
+			} else {
+				
+			}
+		},
+		
+		get : function(property) {
+			if (typeof property == "string") {
+				
+			} else {
+				
+			}
+		}
+	}
+})
