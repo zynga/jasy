e37f10000f31f4696f40f2708f00f848438b7b72
Sebastian Werner <swerner@zynga.com>
2011-08-04 14:28:20 +0200

    REplaced with improved version

--- data/jscore/src/jasy/io/StyleSheet.js
+++ data/jscore/src/jasy/io/StyleSheet.js
@@ -7,56 +7,84 @@
 ==================================================================================================
 */
 
-Module("jasy.io.StyleSheet", 
+Module("jasy.io.StyleSheet",
 {
-	load : (function(global, doc)
-	{
-		var head = doc.getElementsByTagName('head')[0];
+	/**
+	 * Loads a stylesheet and fires a callback when the stylesheet is applied to the page.
+	 *
+	 * Inspired by:
+	 * http://www.phpied.com/when-is-a-stylesheet-really-loaded/
+	 *
+	 * @param {string} url url pointing to the stylesheet
+	 * @param {Function} callback callback that fires when stylesheet is loaded
+	 * @param {Object} context context in which the callback is being executed. Defaults to global context.
+	 */
+	load: function(url, callback, context) {
 
-		var testEl = doc.createElement("link");
-		if ("onload" in testEl)
+		var head = document.getElementsByTagName('head')[0];
+
+		context = context || window;
+
+		// Use listener to stylesheet list and compare elements
+		if (Permutation.isSet("engine", "webkit")) 
 		{
-			return function(url, callback, context)
-			{
-				var el = doc.createElement("link");
-				el.onload = function() 
-				{
-					el.onload = null;
-					callback.call(context||global);
-				}
-				el.type = "stylesheet";
-				el.src = url;
+			var link = document.createElement('link');
+			var sheets = document.styleSheets;
+			var startPos = sheets.length;
 
-				head.appendChild(el);
-			};
-		}
-		else
-		{
-			return function(url, callback, context)
-			{
-				var style = doc.createElement('style');
-				style.textContent = '@import "' + url + '"';
+			handle = setInterval(function() {
 
-				var interval = setInterval(function() 
-				{
-					var succeed = false;
-					try 
-					{
-						// MAGIC: only populated when file is loaded
-						style.sheet.cssRules;
-						succeed = true;
-					} 
-					catch(e){}
+				for (var i = 0, l = sheets.length; i < l; i++)  {
 
-					if (succeed) 
-					{
-						callback.call(context||global);
-						clearInterval(interval);
+					if (sheets[i].ownerNode === link) {
+						clearInterval(handle);
+						callback.call(context);
 					}
-				}, 10);	 
+				}
 
-				head.appendChild(style);
-			}
+			}, 10);
+
+			link.rel = "stylesheet";
+			link.type = "text/css";
+			link.href = url;
+
+			head.appendChild(link);
 		}
-	})(this, document)
+		
+		// Use style import fallback for buggy GECKO 
+		else if (Permutation.isSet("engine", "gecko")) 
+		{
+			var style = document.createElement("style");
+			style.textContent = "@import '" + url + "'";
+
+			var handle = setInterval(function() {
+				try {
+					// MAGIC: only populated when file is loaded
+					style.sheet.cssRules;
+					clearInterval(handle);
+					callback.call(context);
+				} catch(e) {}
+
+			}, 10);
+
+			head.appendChild(style);
+		}
+		
+		// Load event only supported by MSIE and OPERA 
+		else 
+		{
+			var link = document.createElement("link");
+			link.onload = function() {
+				link.onload = null;
+				callback.call(context);
+			};
+
+			link.rel = "stylesheet";
+			link.type = "text/css";
+			link.href = url;
+
+			head.appendChild(link);
+		}
+	}
 });
+
