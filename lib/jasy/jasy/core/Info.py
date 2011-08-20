3d6cb36fc8c0e3c6b3748b2dd66c0cd7ec7b2e1a
Sebastian Werner <info@sebastian-werner.net>
2011-08-20 23:08:00 +0200

    Fixed execution

--- lib/jasy/jasy/core/Info.py
+++ lib/jasy/jasy/core/Info.py
@@ -7,7 +7,7 @@ import os, sys
 
 def root():
     """ Returns the root path of Jasy """
-    return os.path.relpath(os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)))
+    return os.path.relpath(os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, os.pardir)))
     
 def cldrData(what):
     return os.path.join(root(), "external", "cldr", what)
