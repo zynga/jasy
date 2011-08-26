349f4c4f22f8f5c5904e27d2a5dddce3ab2ddc87
Sebastian Werner <swerner@zynga.com>
2011-08-26 15:05:06 +0200

    Implemented first draft

--- data/jscore/src/jasy/property/MGeneric.js
+++ data/jscore/src/jasy/property/MGeneric.js
@@ -5,24 +5,124 @@
 ==================================================================================================
 */
 
-Class("jasy.property.MGeneric",
+(function() 
 {
-	members : 
+	var setters = {};
+	var getters = {};
+	
+	var up = function(name) {
+		return name.charAt(0).toUpperCase() + name.slice(1);
+	};
+	
+	
+	/**
+	 * Generic setter/getter support for property API.
+	 */
+	Class("jasy.property.MGeneric",
 	{
-		set : function(property, value) {
-			if (arguments.length == 2) {
-				
-			} else {
-				
-			}
-		},
-		
-		get : function(property) {
-			if (typeof property == "string") {
-				
-			} else {
-				
+		members : 
+		{
+			/**
+			 * Generic setter. Supports two possible use cases:
+			 *
+			 * <code>
+			 * set(property, value);
+			 * set({
+			 *   property: value
+			 * });
+			 * </code>
+			 *
+			 * @param property {String | Map} Name of property or map of key values
+			 * @param value {var} Any value
+			 * @return {var} Returns the value from the setter (if single property is used)
+			 */
+			set : function(property, value) 
+			{
+				if (arguments.length == 2) 
+				{
+					if (jasy.Env.isSet("debug")) {
+						jasy.Test.assertString(property);
+					}
+
+					var method = setters[property];
+					if (!method) {
+						method = setters[property] = "set" + up(property);
+					}
+					
+					return this[method](value);
+				} 
+				else
+				{
+					if (jasy.Env.isSet("debug")) {
+						jasy.Test.assertMap(property);
+					}
+					
+					for (var i=0, i=property.length; i<l; i++) 
+					{
+						
+						var name = property[i];
+						var method = setters[name];
+						if (!method) {
+							method = setters[name] = "set" + up(name);
+						}
+
+						this[method](property[value]);
+					}
+				}
+			},
+
+
+			/**
+			 * Generic getter. Supports two possible use cases:
+			 *
+			 * <code>
+			 * get(property);
+			 * get(["width", "height"]);
+			 * </code>
+			 *
+			 * @param property {String | Map} Name of property or map of key values
+			 * @param value {var} Any value
+			 * @return {var} Returns the value from the setter (if single property is used)
+			 */
+			get : function(property) 
+			{
+				if (typeof property == "string") 
+				{
+					if (jasy.Env.isSet("debug")) {
+						jasy.Test.assertString(property);
+					}
+
+					var method = getters[property];
+					if (!method) {
+						method = getters[property] = "get" + up(property);
+					}
+					
+					return this[method]();
+				} 
+				else 
+				{
+					if (jasy.Env.isSet("debug")) {
+						jasy.Test.assertArray(property);
+					}
+					
+					var ret = {};
+					
+					for (var i=0, i=property.length; i<l; i++) 
+					{
+						
+						var name = property[i];
+						var method = getters[name];
+						if (!method) {
+							method = getters[name] = "get" + up(name);
+						}
+
+						ret[name] = this[method]();
+					}
+					
+					return ret;
+				}
 			}
 		}
-	}
-})
+	});
+})();
+
