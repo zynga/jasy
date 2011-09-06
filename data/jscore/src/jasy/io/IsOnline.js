4dfdc6ab8845a52edf2da601bd31f00320468578
Sebastian Werner <swerner@zynga.com>
2011-09-06 16:20:41 +0200

    Reworked network monitoring

--- data/jscore/src/jasy/io/IsOnline.js
+++ data/jscore/src/jasy/io/IsOnline.js
@@ -7,50 +7,113 @@
 ==================================================================================================
 */
 
-// 
+(function(global) 
+{
+	// Detect native support
+	var XHR = global.XMLHttpRequest;
+	var empty = new Function;
+	var isOnline = false;
+	var request;
 
-function isOnline() {
+	var updateHandle = null;
+	var updateInterval = 1000;
 
-	// IE vs. standard XHR creation
+	var abortHandle = null;
+	var abortAfter = 5000;
+	
+	
+	var setOnline = function(value) 
+	{
+		isOnline = value;
+		console.debug("IsOnline: " + isOnline);
+		updateHandle = setTimeout(detectOnline, updateInterval);
+	};
+	
+	
+	var abortRequest = function() 
+	{
+		request.onreadystatechange = empty;
+		request.abort();
+		
+		setOnline(false);
+	};
+	
+	
+	var onStateChange = function(e) 
+	{
+		if (request.readyState == 2) {
+			abortHandle = setTimeout(abortRequest, abortAfter);
+		}
+		
+		if (request.readyState == 4) 
+		{
+			request.onreadystatechange = empty;
+			clearTimeout(abortHandle);
 
-	var x = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" ),
+			var status = request.status;
+			setOnline(status >= 200 && status < 300 || status == 304 || status == 1223);
+		}
+	};
+	
+	
+	var detectOnline = function() 
+	{
+		// IE vs. standard XHR creation
+		request = XHR ? new XHR : new ActiveXObject("Microsoft.XMLHTTP");
+		request.onreadystatechange = onStateChange;
+		request.open("HEAD", "//" + location.hostname + "/?rand=" + Math.random(), true);
+		
+		// Catch network & other problems
+		try {
+			request.send();
+		} catch (e) {
+			setOnline(false);
+		}
+	};
+	
+	
+	/**
+	 * Generic network monitor and inspection
+	 *
+	 */
+	Module("jasy.io.Network", 
+	{
+		/**
+		 * Returns whether the client is online based on the last check
+		 * 
+		 * @return {Boolean} Whether the client is online
+		 */
+		isOnline : function() {
+			return updateHandle ? isOnline : null;
+		},
+		
 
-			s;
+		/**
+		 * Starts the network monitoring
+		 *
+		 * @param interval {Number} Number of milliseconds between the update requests
+		 */
+		startMonitoring : function(interval) 
+		{
+			// allow for timeout reconfiguration
+			stopMonitoring();
+			
+			// Reconfigure timeout and start first iteration
+			updateInterval = interval || 1000;
+			updateHandle = setTimeout(detectOnline, updateInterval);
+		},
+		
 
-	x.open(
-
-		// requesting the headers is faster, and just enough
-
-		"HEAD",
-
-		// append a random string to the current hostname,
-
-		// to make sure we're not hitting the cache
-
-		"//" + window.location.hostname + "/?rand=" + Math.random(),
-
-		// make a synchronous request
-
-		false
-
-	);
-
-	try {
-
-		x.send();
-
-		s = x.status;
-
-		// Make sure the server is reachable
-
-		return ( s >= 200 && s < 300 || s === 304 );
-
-	// catch network & other problems
-
-	} catch (e) {
-
-		return false;
-
-	}
-
-}
+		/**
+		 * Stop network monitoring
+		 */
+		stopMonitoring : function() 
+		{
+			if (updateHandle) 
+			{
+				clearTimeout(updateHandle);
+				updateHandle = null;
+			}
+		}
+	});
+})(this);
