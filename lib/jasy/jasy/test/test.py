ce386557263514734c4dec4f59e8d02035d7f9ae
Sebastian Werner <swerner@zynga.com>
2011-09-12 17:00:00 +0200

    Cleanup

--- lib/jasy/jasy/test/test.py
+++ lib/jasy/jasy/test/test.py
@@ -24,48 +24,8 @@ def variableoptimize(code):
     Variables.scan(node)
     LocalVariables.optimize(node, node.stats)
     return Compressor.compress(node)
-    
-
-class TestParser(unittest.TestCase):
-    
-    def setUp(self):
-        pass
-        
-        
-    def test_function(self):
-        self.assertEqual(
-            parse("function abc() { return 1; }"),
-            '<script line="0"><function functionForm="declared_form" line="0" name="abc"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></script>'
-        )
-
-        self.assertEqual(
-            parse("var abc = function() { return 1; }"),
-            '<script line="0"><var line="0"><declaration line="0" name="abc" readOnly="false"><initializer><function functionForm="expressed_form" line="0"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></initializer></declaration></var></script>'
-        )
-        
-        self.assertEqual(
-            parse("var abc = function abc() { return 1; }"),
-            '<script line="0"><var line="0"><declaration line="0" name="abc" readOnly="false"><initializer><function functionForm="expressed_form" line="0" name="abc"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></initializer></declaration></var></script>'
-        )
-        
-        
-    def test_expression(self):
-        
-        self.assertEqual(
-            parse('a + ++i;'),
-            '<script line="0"><semicolon line="0"><expression><plus line="0"><identifier line="0" value="a"/><increment line="0"><identifier line="0" value="i"/></increment></plus></expression></semicolon></script>'
-        )
-        
-        
-        self.assertEqual(
-            parse('a++ + i;'),
-            '<script line="0"><semicolon line="0"><expression><plus line="0"><increment line="0" postfix="true"><identifier line="0" value="a"/></increment><identifier line="0" value="i"/></plus></expression></semicolon></script>'
-        )
-        
-        
 
 
-    
 
 
 class TestCompressor(unittest.TestCase):
