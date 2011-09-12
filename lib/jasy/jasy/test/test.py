44214b2f09ba74e1b2cf2619c3d6cf9e6b427ebd
Sebastian Werner <swerner@zynga.com>
2011-09-12 13:17:24 +0200

    Aded more tests

--- lib/jasy/jasy/test/test.py
+++ lib/jasy/jasy/test/test.py
@@ -64,14 +64,88 @@ class TestCompressor(unittest.TestCase):
     def setUp(self):
         pass
         
-        
     def test_and(self):
         self.assertEqual(compress('x && y'), 'x&&y;')
 
     def test_arithm(self):
         self.assertEqual(compress('i++; j-- + 3;'), 'i++;j--+3;')
+
+    def test_array_number(self):
+        self.assertEqual(compress('var data1 = [ 1, 2, 3 ];'), 'var data1=[1,2,3];')
+
+    def test_array_string(self):
+        self.assertEqual(compress('var data2 = [ "hello" ];'), 'var data2=["hello"];')
+
+    def test_array_sparse(self):
+        self.assertEqual(compress('var data3 = [ 1, , , 4, , 6 ];'), 'var data3=[1,,,4,,6];')
+        
+    def test_array_comprehension(self):
+        self.assertEqual(compress('exec([i for (i in obj) if (i > 3)]);'), 'exec([i for(i in obj)if(i>3)]);')
+
+    def test_bitwise_and(self):
+        self.assertEqual(compress('z = x & y;'), 'z=x&y;')
+
+    def test_block_separate(self):
+        self.assertEqual(compress('{ x = 1; y = 2; }'), '{x=1;y=2}')
+
+    def test_block_empty(self):
+        self.assertEqual(compress('if (true) {}'), 'if(true){}')
+
+    def test_call_singlearg(self):
+        self.assertEqual(compress('hello("hello world");'), 'hello("hello world");')
+
+    def test_call_multiargs(self):
+        self.assertEqual(compress('multi(1, 2, 3);'), 'multi(1,2,3);')
+
+    def test_call_destruct(self):
+        self.assertEqual(compress('[a, b] = f();'), '[a,b]=f();')
+
+    def test_const(self):
+        self.assertEqual(compress('const foo = 3;'), 'const foo=3;')
+
+    def test_const_multi(self):
+        self.assertEqual(compress('const foo = 3, bar = 4;'), 'const foo=3,bar=4;')
+
+    def test_continue(self):
+        self.assertEqual(compress('while(x) { continue; }'), 'while(x){continue}')
+
+    def test_continue_label(self):
+        self.assertEqual(compress('dist: while(y) { continue dist; }'), 'dist:while(y){continue dist};')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+    def test_(self):
+        self.assertEqual(compress(''), '')
+
+
         
         
+        
+        
+        
+
 
 
 if __name__ == '__main__':
