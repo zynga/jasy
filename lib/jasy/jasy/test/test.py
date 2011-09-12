43fce1aa1b3b79c917462440466331c5c83d9a65
Sebastian Werner <swerner@zynga.com>
2011-09-12 12:35:16 +0200

    Added compressor unit tests

--- lib/jasy/jasy/test/test.py
+++ lib/jasy/jasy/test/test.py
@@ -6,7 +6,15 @@ import sys, os, unittest
 jasyroot = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir, os.pardir))
 sys.path.insert(0, jasyroot)
 
-from jasy.parser.Parser import parse
+import jasy.parser.Parser as Parser
+import jasy.process.Compressor as Compressor
+
+def parse(code):
+    return Parser.parse(code).toXml(False)
+    
+def compress(code):
+    return Compressor.compress(Parser.parse(code))
+
 
 
 class TestParser(unittest.TestCase):
@@ -17,17 +25,17 @@ class TestParser(unittest.TestCase):
         
     def test_function(self):
         self.assertEqual(
-            parse("function abc() { return 1; }").toXml(False),
+            parse("function abc() { return 1; }"),
             '<script line="0"><function functionForm="declared_form" line="0" name="abc"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></script>'
         )
 
         self.assertEqual(
-            parse("var abc = function() { return 1; }").toXml(False),
+            parse("var abc = function() { return 1; }"),
             '<script line="0"><var line="0"><declaration line="0" name="abc" readOnly="false"><initializer><function functionForm="expressed_form" line="0"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></initializer></declaration></var></script>'
         )
         
         self.assertEqual(
-            parse("var abc = function abc() { return 1; }").toXml(False),
+            parse("var abc = function abc() { return 1; }"),
             '<script line="0"><var line="0"><declaration line="0" name="abc" readOnly="false"><initializer><function functionForm="expressed_form" line="0" name="abc"><body><script line="0"><return line="0"><value><number line="0" value="1"/></value></return></script></body></function></initializer></declaration></var></script>'
         )
         
@@ -35,15 +43,20 @@ class TestParser(unittest.TestCase):
     def test_expression(self):
         
         self.assertEqual(
-            parse('a + ++i;').toXml(False),
+            parse('a + ++i;'),
             '<script line="0"><semicolon line="0"><expression><plus line="0"><identifier line="0" value="a"/><increment line="0"><identifier line="0" value="i"/></increment></plus></expression></semicolon></script>'
         )
         
         
         self.assertEqual(
-            parse('a++ + i;').toXml(False),
+            parse('a++ + i;'),
             '<script line="0"><semicolon line="0"><expression><plus line="0"><increment line="0" postfix="true"><identifier line="0" value="a"/></increment><identifier line="0" value="i"/></plus></expression></semicolon></script>'
         )
+        
+        
+
+
+    
 
 
 class TestCompressor(unittest.TestCase):
@@ -52,8 +65,11 @@ class TestCompressor(unittest.TestCase):
         pass
         
         
-    def test_123(self):
-        pass
+    def test_and(self):
+        self.assertEqual(compress('x && y'), 'x&&y;')
+
+    def test_arithm(self):
+        self.assertEqual(compress('i++; j-- + 3;'), 'i++;j--+3;')
         
         
 
