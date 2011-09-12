29107ec6d001d9dc812a028094a77edd2c237cf4
Sebastian Werner <swerner@zynga.com>
2011-09-12 12:02:44 +0200

    Added new unittest based testing

---
+++ lib/jasy/jasy/test/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class TestParser(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+        
+        
+    def test_123(self):
+        pass
+
+
+
+class TestCompressor(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+        
+    def test_123(self):
+        pass
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
