090355c6ce84c8bfbfbdaf3e19918e6ed934f0e7
Sebastian Werner <swerner@zynga.com>
2011-11-02 19:53:07 +0100

    Fixed jasy paths

--- setup.py
+++ setup.py
@@ -15,14 +15,51 @@ version = "0.7"
 setup(
       name = 'jasy',
       version = version,
+      
       author = 'Sebastian Werner',
       author_email = 'info@sebastian-werner.net',
+      
       url = 'http://github.com/wpbasti/jasy',
       download_url = "http://github.com/downloads/wpbasti/jasy/jasy-%s.tar.gz" % version,
+      
       license = "http://www.apache.org/licenses/LICENSE-2.0",
+      
       description = "Jasy is a build system for JavaScript focused web projects.",
-      packages = ['jasy','jasy.core','jasy.ext','jasy.optimizer','jasy.parser','jasy.process','jasy.tokenizer'],
-      package_dir = {'': 'lib'},
-      package_data = {'jasy': ['data/cldr/VERSION', 'data/cldr/keys/*.xml', 'data/cldr/main/*.xml', 'data/cldr/supplemental/*.xml']},
-      scripts = ["bin/jasy", "bin/jscompress", "bin/jsdeps", "bin/jsmeta", "bin/jsoptimize", "bin/jstree"]
+      long_description = "",
+
+      packages = [
+        'jasy',
+        'jasy.core',
+        'jasy.ext',
+        'jasy.optimizer',
+        'jasy.parser',
+        'jasy.process',
+        'jasy.tokenizer'
+      ],
+      package_dir = {
+        '': 'lib'
+      },
+      package_data = {
+        'jasy': [
+          'data/cldr/VERSION', 
+          'data/cldr/keys/*.xml', 
+          'data/cldr/main/*.xml', 
+          'data/cldr/supplemental/*.xml'
+        ]
+      },
+      scripts = [
+        "bin/jasy", 
+        "bin/jscompress", 
+        "bin/jsdeps", 
+        "bin/jsmeta", 
+        "bin/jsoptimize", 
+        "bin/jstree"
+      ],
+      data_files = [
+        ("doc", [
+          "readme.md", 
+          "license-mit.md", 
+          "license-apache.md"
+        ])
+      ]
 )
