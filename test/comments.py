fc88ae99da8b802de66af7cd8306785c6bd2a84d
Sebastian Werner <swerner@zynga.com>
2012-01-03 13:38:45 +0100

    Removed old test

--- test/comments.py
+++ test/comments.py
@@ -508,52 +508,7 @@ class TestComments(unittest.TestCase):
     #
 
     
-    
-    def xtest_class_decl(self):
-        self.process('''
-        /**
-         * Class documentation
-         */
-        core.Class.define("my.custom.Class",
-        {
-          /**
-           * @param x 
-           */
-          construct : function(x) {
 
-          },
-
-
-          /* 
-          *********************************************
-             MEMBERS
-          ********************************************* 
-          */
-
-          members :
-          {
-            /**
-             * A really nice method
-             */
-            method : function(/** Object */ param1, /** String */ param2, /** my.other.Class */ param3)
-            {
-              // multi line
-              // single comments
-              doSomething();
-
-              /* detected as a section */
-              blockElem1();
-              blockElem2();
-              blockElem3();
-              /* detected as a block */
-              doElse();
-
-              var complex = 1 * 2 * 3; // inline attached to previous statement
-              var simple = 1 * 2;
-            }
-          }
-        });
-        ''')
         
         
 if __name__ == '__main__':
