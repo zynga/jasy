106dab20d204427a19790cd270e052cb77434cd7
Sebastian Werner <swerner@zynga.com>
2012-01-05 13:20:47 +0100

    Bumped version number

--- lib/jasy/__init__.py
+++ lib/jasy/__init__.py
@@ -24,7 +24,7 @@ from jasy.js.output.Formatting import *
 from jasy.util.File import *
 
 # Current version. Used by setuptools
-__version__ = "0.4.6"
+__version__ = "0.5-alpha1"
 
 # Export only main routine
 __all__ = ["main", "__version__"]
