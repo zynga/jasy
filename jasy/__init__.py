f75610cd87f947187946db66fa1ec275ab8fee03
Sebastian Werner <swerner@zynga.com>
2012-01-18 15:16:35 +0100

    Fixed jasy

--- jasy/__init__.py
+++ jasy/__init__.py
@@ -3,27 +3,5 @@
 # Copyright 2010-2012 Sebastian Werner
 #
 
-# Current version. Used by setuptools
 __version__ = "0.5-alpha5"
-
-# Export only main routine
-__all__ = ["__version__"]
-
-from jasy.core.Error import *
-from jasy.core.Task import *
-from jasy.core.Session import *
-from jasy.core.Project import *
-
-from jasy.asset.Asset import * 
-
-from jasy.js.Resolver import *
-from jasy.js.Sorter import *
-
-from jasy.js.api.Writer import *
-
-from jasy.js.output.Combiner import *
-from jasy.js.output.Optimization import *
-from jasy.js.output.Formatting import *
-
-from jasy.util.File import *
-
+__author__ = "Sebastian Werner <info@sebastian-werner.net>"
