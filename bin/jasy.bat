5f3714ad7c97672ebc660e962a85d5eca0c1f029
Sebastian Werner <swerner@zynga.com>
2012-02-17 11:41:09 +0100

    Updated bat scripts

--- bin/jasy.bat
+++ bin/jasy.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 SET THISDIR=%~DP0
 FOR %%X IN (python3.exe) DO (SET FOUND=%%~$PATH:X)
-IF DEFINED FOUND python3 "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9 ELSE python "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9
+IF DEFINED FOUND python3 "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9
+IF NOT DEFINED FOUND python "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9
