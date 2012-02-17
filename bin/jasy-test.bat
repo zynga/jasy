d78bfb421f9dbd3a326ed50afafa09af38176e02
Sebastian Werner <swerner@zynga.com>
2012-02-17 10:55:41 +0100

    Updated batch scripts to better deal with Python3. Added wrappers for other binaries, too.

---
+++ bin/jasy-test.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+SET THISDIR=%~DP0
+FOR %%X IN (python3.exe) DO (SET FOUND=%%~$PATH:X)
+IF DEFINED FOUND python3 "%THISDIR%\jasy-test" %1 %2 %3 %4 %5 %6 %7 %8 %9 ELSE python "%THISDIR%\jasy-test" %1 %2 %3 %4 %5 %6 %7 %8 %9
--- bin/jasy.bat
+++ bin/jasy.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 SET THISDIR=%~DP0
-python "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9
+FOR %%X IN (python3.exe) DO (SET FOUND=%%~$PATH:X)
+IF DEFINED FOUND python3 "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9 ELSE python "%THISDIR%\jasy" %1 %2 %3 %4 %5 %6 %7 %8 %9
