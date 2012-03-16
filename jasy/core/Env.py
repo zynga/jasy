3d90296c04c10a43bba60a91df5c027ef461df5d
Sebastian Werner <swerner@zynga.com>
2012-03-16 14:52:06 +0100

    Some docs

--- jasy/core/Env.py
+++ jasy/core/Env.py
@@ -16,18 +16,27 @@ from jasy.core.Session import Session
 
 session = Session()
 
+# Task API for user scripts
 from jasy.core.Task import *
 from jasy.asset.Asset import * 
 
+# Resolving/Sorting classes
 from jasy.js.Resolver import Resolver
 from jasy.js.Sorter import Sorter
 
+# API Writer
 from jasy.js.api.Writer import ApiWriter
 
+# Output options
 from jasy.js.output.Optimization import Optimization
 from jasy.js.output.Formatting import Formatting
 
+formatting = Formatting()
+optimization = Optimization()
+
+# Import file operation goodies
 from jasy.util.File import *
 
+# Import combiner script for merging/write js output
 from jasy.js.output.Combiner import *
 
