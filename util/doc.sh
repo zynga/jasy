19b4056705e705458be023202b050561fe624001
Sebastian Werner <swerner@zynga.com>
2012-03-23 15:39:04 +0100

    Worked on doc generator

--- util/doc.sh
+++ util/doc.sh
@@ -2,6 +2,7 @@
 
 cd `dirname $0`/..
 rm -rf doc
-sphinx-apidoc --full --maxdepth 6 --output-dir doc jasy
+sphinx-apidoc --full --maxdepth 8 --output-dir doc jasy
+cp -f util/doc-conf.py doc/conf.py
 cd doc
 make html
