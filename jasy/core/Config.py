98b7a91d6979d24bc1e3c1ff7ac95c473277596f
Sebastian Werner <swerner@zynga.com>
2012-08-21 12:45:14 +0200

    Added support for finding, loading and removing config files. Improved stability.

--- jasy/core/Config.py
+++ jasy/core/Config.py
@@ -6,17 +6,65 @@ try:
 except ImportError:
     yaml = None
 
-def writeConfig(data, filename, indent=2, encoding="utf-8"):
-    fileExt = os.path.splitext(filename)[1]
-    fileHandle = open(filename, mode="w", encoding="utf-8")
 
+def findConfig(fileName):
+    fileExt = os.path.splitext(fileName)[1]
+
+    # Auto discovery
+    if not fileExt:
+        for tryExt in (".json", ".yaml"):
+            if os.path.exists(fileName + tryExt):
+                return fileName + tryExt
+
+        return None  
+
+    if os.path.exists(fileName) and fileExt in (".json", ".yaml"):
+        return fileName  
+    else:
+        return None
+
+
+def removeConfig(fileName):
+    configName = findConfig(fileName)
+    if configName is not None:
+        os.remove(configName)
+        return True
+
+    return False
+
+
+def loadConfig(fileName, encoding="utf-8"):
+    configName = findConfig(fileName)
+    if configName is None:
+        raise JasyError("Unsupported config file: %s" % fileName)
+
+    fileHandle = open(configName, mode="r", encoding=encoding)    
+
+    fileExt = os.path.splitext(configName)[1]
+    if fileExt == ".json":
+        return json.load(fileHandle)
+
+    elif fileExt == ".yaml":
+        if yaml is None:
+            raise JasyError("Unable to safe YAML. Python module is missing!")
+
+        return yaml.load(fileHandle)
+        
+
+
+def writeConfig(data, fileName, indent=2, encoding="utf-8"):
+    fileHandle = open(fileName, mode="w", encoding=encoding)
+
+    fileExt = os.path.splitext(fileName)[1]
     if fileExt == ".json":
         json.dump(data, fileHandle, indent=indent, ensure_ascii=False)
+    
     elif fileExt == ".yaml":
         if yaml is None:
             raise JasyError("Unable to safe YAML. Python module is missing!")
 
         yaml.dump(data, fileHandle, default_flow_style=False, indent=indent, allow_unicode=True)
+
     else:
-        raise JasyError("Unsupported file type: %s" % fileExt)
+        raise JasyError("Unsupported config type: %s" % fileExt)
 
