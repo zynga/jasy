5f2edfb91f6392f70b4cb0839b7942494be68239
Sebastian Werner <swerner@zynga.com>
2012-08-23 13:58:24 +0200

    Updated setup script to prefer distribute. Re-added install_requires section.

--- setup.py
+++ setup.py
@@ -7,18 +7,38 @@
 
 import sys
 
-major, minor = sys.version_info[:2]
-
-if major < 3:
-  print("Jasy requires Python 3")
+if sys.version < "3.2":
+  print("Jasy requires Python 3.2 or higher")
   sys.exit(1)
 
-# Load distutils and switch to distribute afterwards
-from distutils.core import setup
-
+# Prefer setuptools (aka distribute) over distutils 
+# - Distutils comes with Python3 but is not capable of installing requires, extras, etc.
+# - Distribute is a fork of the Setuptools project (http://packages.python.org/distribute/)
+try:
+  from setuptools import setup
+except ImportError:
+  print("Jasy prefers distribute over distutils for installing dependencies!")
+  from distutils.core import setup
+  
 # Import Jasy for version info etc.
 import jasy
 
+# Basic non native requirements
+install_requires = [ 
+  "Pygments>=1.5", 
+  "polib>=1.0", 
+  "requests>=0.13", 
+  "CherryPy>=3.2", 
+  "PyYAML>=3" 
+ ]
+
+# Integrate batch script for win32 only
+if sys.platform == "win32":
+  scripts = [ "bin/jasy", "bin/jasy-test",  "bin/jasy-util", "bin/jasy.bat", "bin/jasy-test.bat", "bin/jasy-util.bat" ]
+else:
+  scripts = [ "bin/jasy", "bin/jasy-test",  "bin/jasy-util" ]
+
+# Run setup
 setup(
   name = 'jasy',
   version = jasy.__version__,
@@ -26,24 +46,46 @@ setup(
   author = 'Zynga Inc.',
   author_email = 'germany@zynga.com',
 
-  url = 'http://github.com/wpbasti/jasy',
+  maintainer = 'Zynga Inc.',
+  maintainer_email = 'germany@zynga.com',
+
+  url = 'http://github.com/zynga/jasy',
   download_url = "http://pypi.python.org/packages/source/j/jasy/jasy-%s.zip" % jasy.__version__,
 
-  license = "Apache v2.0",
+  license = "MIT",
   platforms = 'any',
 
   description = "Web Tooling Framework",
   long_description = open('readme.md').read(),
 
+  install_requires = install_requires,
+
+  test_suite = "jasy.test",
+
+  extras_require = {
+    "jsdoc" : ["misaka"],
+    "daemon" : ["watchdog"],
+    "sprites" : ["pil"]
+  },
+
+  dependency_links = [
+    "https://github.com/sloonz/pil-py3k",
+    "https://github.com/wpbasti/watchdog"
+  ],
+
   # Via: http://pypi.python.org/pypi?%3Aaction=list_classifiers
   classifiers = [
 
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
+    'License :: OSI Approved :: MIT License',
+    'License :: Freely Distributable',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
     'Topic :: Documentation',
     'Topic :: Software Development :: Build Tools',
     'Topic :: Software Development :: Compilers',
@@ -51,7 +93,8 @@ setup(
     'Topic :: Software Development :: Internationalization',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Software Development :: Localization',
-    'Topic :: Software Development :: Testing'
+    'Topic :: Software Development :: Testing',
+    'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
 
   ],
 
@@ -83,10 +126,10 @@ setup(
     ]
   },
 
-  scripts = [ "bin/jasy", "bin/jasy-test",  "bin/jasy-util", "bin/jasy.bat", "bin/jasy-test.bat", "bin/jasy-util.bat" ],
+  scripts = scripts,
 
   data_files = [
-    ("doc/jasy-%s" % jasy.__version__, [
+    ("jasy", [
       "changelog.md",
       "license.md",
       "readme.md",
