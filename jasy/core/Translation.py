cbcf457cc808bfda93eeec24a55a0c889f643010
Sebastian Werner <swerner@zynga.com>
2012-09-04 12:36:33 +0200

    Worked on translation support

--- jasy/core/Translation.py
+++ jasy/core/Translation.py
@@ -6,5 +6,22 @@
 from jasy.core.Item import Item
 
 class Translation(Item):
-    pass
+
+    def __init__(self, project, id=None):
+        self.id = id
+        self.project = project
+
+        # Extract language from file ID
+        # Thinking of that all files are named like de.po, de.txt, de.properties, etc.
+        lang = self.id
+        if "." in lang:
+            lang = lang[lang.rfind(".")+1:]
+
+        self.language = lang
+
+    
+    def getLanguage(self):
+        """Returns the language of the translation file"""
+        
+        return self.language
 
