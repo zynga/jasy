cfb3f12bcdb79b7dce25f520d8beb831dec1c8e8
Sebastian Werner <swerner@zynga.com>
2012-09-11 14:14:18 +0200

    Added docs

---
+++ jasy/item/__init__.py
@@ -0,0 +1,11 @@
+import jasy.item.Asset
+import jasy.item.Class
+import jasy.item.Doc
+import jasy.item.Item
+import jasy.item.Translation
+
+Asset = jasy.item.Asset.Asset
+Class = jasy.item.Asset.Class
+Doc = jasy.item.Asset.Doc
+Item = jasy.item.Asset.Item
+Translation = jasy.item.Asset.Translation
