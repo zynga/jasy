e1826ad4018cc83a03eec51c757ce03945b543ee
Sebastian Werner <swerner@zynga.com>
2012-09-12 15:31:54 +0200

    Cleanup

--- jasy/env/Context.py
+++ jasy/env/Context.py
@@ -3,19 +3,23 @@
 # Copyright 2010-2012 Zynga Inc.
 #
 
-from jasy import UserError
-import jasy.env.Task
-from jasy.env.Config import Config
-from jasy.core.Logging import *
-from jasy import UserError
-from jasy.core.Json import toJson
-from jasy.env.Task import task, executeTask, runTask
-from jasy.env.File import *
+# Modules
+import jasy.core.Logging as Logging
+
+# Classes
 from jasy.asset.SpritePacker import SpritePacker
 from jasy.js.Resolver import Resolver
 from jasy.js.api.Writer import ApiWriter
+from jasy.env.Config import Config
+from jasy import UserError
+
+# Commands
+# TODO: Move them into modules
+from jasy.env.Task import task, executeTask, runTask
+from jasy.env.File import *
 from jasy.http.Server import serve
 from jasy.core.Util import executeCommand
+from jasy.env.JavaScript import *
 
 # from jasy.js.output.Optimization import Optimization
 # from jasy.js.output.Formatting import Formatting
@@ -26,10 +30,4 @@ from jasy.core.Util import executeCommand
 # # Unimport classes
 # del Formatting
 # del Optimization
-
-# Handy utility methods to process class lists
-from jasy.env.JavaScript import *
-
-
-
 
