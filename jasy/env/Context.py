949af9641ef83d892921338f9225cdb1dcdf18e6
Sebastian Werner <swerner@zynga.com>
2012-09-12 11:14:53 +0200

    Moved jasyscript.py environment into Context.py

---
+++ jasy/env/Context.py
@@ -0,0 +1,39 @@
+#
+# Jasy - Web Tooling Framework
+# Copyright 2010-2012 Zynga Inc.
+#
+
+# Import global environment
+from jasy.env.State import session
+
+# Configuring for remote task calls
+from jasy import UserError
+import jasy.env.Task
+from jasy.env.Config import Config
+import jasy.env.Builtin
+from jasy.core.Logging import *
+from jasy import UserError
+from jasy.core.Json import *
+from jasy.env.Task import task, executeTask, runTask
+from jasy.env.File import *
+from jasy.asset.SpritePacker import SpritePacker
+from jasy.js.Resolver import Resolver
+from jasy.js.api.Writer import ApiWriter
+from jasy.http.Server import serve
+from jasy.core.Util import executeCommand
+
+# from jasy.js.output.Optimization import Optimization
+# from jasy.js.output.Formatting import Formatting
+# 
+# jsFormatting = Formatting()
+# jsOptimization = Optimization("variables", "declarations", "blocks", "privates")
+# 
+# # Unimport classes
+# del Formatting
+# del Optimization
+
+# Handy utility methods to process class lists
+from jasy.env.JavaScript import *
+
+
+
