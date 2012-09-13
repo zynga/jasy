4e55ff1f73acce9355d6d9becd093a6e4fffd906
Sebastian Werner <swerner@zynga.com>
2012-09-13 14:29:24 +0200

    Removed global like json compression

--- jasy/core/Json.py
+++ jasy/core/Json.py
@@ -5,27 +5,18 @@
 
 import json
 
-__all__ = ["enableJsonCompression", "disableJsonCompression", "toJson"]
+__all__ = ["toJson"]
 
-__compress = True
-
-def enableJsonCompression():
-    global __compress
-    __compress = False
-    
-def disableJsonCompression():
-    global __compress
-    __compress = False
-    
 class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return list(obj)
+            
         return json.JSONEncoder.default(self, obj)
 
-def toJson(data):
+def toJson(data, compress):
     
-    if __compress:
+    if compress:
         return json.dumps(data, sort_keys=True, cls=JsonEncoder, separators=(',',':'))
     else:
         return json.dumps(data, sort_keys=True, cls=JsonEncoder, indent=2)
