2c094a18632fc38ea0bb30bac65b364de45720d3
Sebastian Werner <swerner@zynga.com>
2012-09-14 15:19:54 +0200

    Minor changes

--- jasy/__init__.py
+++ jasy/__init__.py
@@ -3,7 +3,7 @@
 # Copyright 2010-2012 Zynga Inc.
 #
 
-__version__ = "0.8-beta4"
+__version__ = "0.8-beta5"
 __author__ = "Sebastian Werner <info@sebastian-werner.net>"
 
 import os.path
@@ -12,7 +12,7 @@ datadir = os.path.join(os.path.dirname(__file__), "data")
 def info():
     import jasy.core.Console as Console
 
-    print("Jasy %s is powerful web tooling framework inspired by SCons" % __version__)
+    print("Jasy %s is a powerful web tooling framework" % __version__)
     print("Copyright (c) 2010-2012 Zynga Inc. %s" % Console.colorize("http://zynga.com/", "underline"))
     print("Visit %s for details." % Console.colorize("https://github.com/zynga/jasy", "underline"))
     print()
