d12f54eaabe64a1b730636cfedcc839cb5d32034
Sebastian Werner <swerner@zynga.com>
2012-09-17 11:32:37 +0200

    Added new doc scripts

---
+++ bin/jasy-doc.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+SET THISDIR=%~DP0
+FOR %%X IN (python3.exe) DO (SET FOUND=%%~$PATH:X)
+IF DEFINED FOUND python3 "%THISDIR%\jasy-doc" %1 %2 %3 %4 %5 %6 %7 %8 %9
+IF NOT DEFINED FOUND python "%THISDIR%\jasy-doc" %1 %2 %3 %4 %5 %6 %7 %8 %9
