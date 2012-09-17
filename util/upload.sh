c62801c21fa2781b3976e689377e49a31ab62667
Sebastian Werner <swerner@zynga.com>
2012-09-17 17:03:50 +0200

    Minor fixes

--- util/upload.sh
+++ util/upload.sh
@@ -3,10 +3,10 @@
 cd `dirname $0`/..
 version=`python3 -c "import jasy; print(jasy.__version__)"`
 
-python3 setup.py test > /dev/null
-python3 setup.py clean
+python3 setup.py test > /dev/null || exit 1
+python3 setup.py clean || exit 1
 
-python3 setup.py sdist --formats zip upload
+python3 setup.py sdist --formats zip upload || exit 1
 
-python3 bin/jasy-doc
-python3 setup.py upload_docs --upload-dir=docs
+python3 bin/jasy-doc || exit 1
+python3 setup.py upload_docs --upload-dir=docs || exit 1
