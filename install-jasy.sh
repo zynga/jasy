7db42d2d079a2a2e0b983178a4718f1d65cbc226
Dali Kilani <dkilani@zynga.com>
2012-10-10 17:07:35 -0500

    More cleanup and improvement to event tracking

--- install-jasy.sh
+++ install-jasy.sh
@@ -12,49 +12,52 @@
 MIXPANEL_TOKEN="cdd7359ea0c86f5b58f074f369829e41"
 
 function track_event {
-# $1 : EVENT_NAME
-
-if [ -z $1 ]
-then
-	EVENT_NAME= "unknown"
-else
-	EVENT_NAME=$1
-fi
-
-# $2 : extra properties to be passed to Mixpanel
-# Format is : \"PROPERTY1_NAME\" : \"PROPERTY1_VALUE\",\"PROPERTY2_NAME\" : \"PROPERTY2_VALUE\"
-if [ -n $2 ]
-then
-	TRACK_DATA_EXTRA_PROPERTIES=",$2"
-else
-	TRACK_DATA_EXTRA_PROPERTIES=""
-fi
-
-TRACK_DATA_PROPERTIES="\"properties\":{\"token\":\"$MIXPANEL_TOKEN\",\"distinct_id\":\"$DISTINCT_ID\" $TRACK_DATA_EXTRA_PROPERTIES}"
-TRACK_DATA="{$TRACK_DATA_PROPERTIES,\"event\":\"$EVENT_NAME\"}"
-ENCODED_TRACK_DATA=`echo $TRACK_DATA | base64`
-MIXPANEL_API_ENDPOINT="https://api.mixpanel.com/track/?data=$ENCODED_TRACK_DATA"
-
-if [ -n $ENABLE_TRACKING ]
-then
-	curl -s -X POST $MIXPANEL_API_ENDPOINT > /dev/null
-fi
+  # $1 : EVENT_NAME
+  
+  if [ -z "$1" ]
+  then
+  	EVENT_NAME= "unknown"
+  else
+  	EVENT_NAME=$1
+  fi
+  
+  # $2 : extra properties to be passed to Mixpanel
+  # Format is : \"PROPERTY1_NAME\" : \"PROPERTY1_VALUE\",\"PROPERTY2_NAME\" : \"PROPERTY2_VALUE\"
+  if [ -n "$2" ]
+  then
+  	TRACK_DATA_EXTRA_PROPERTIES=",$2"
+  else
+  	TRACK_DATA_EXTRA_PROPERTIES=""
+  fi
+  
+  TRACK_DATA_PROPERTIES="\"properties\":{\"token\":\"$MIXPANEL_TOKEN\",\"distinct_id\":\"$DISTINCT_ID\" $TRACK_DATA_EXTRA_PROPERTIES}"
+  TRACK_DATA="{$TRACK_DATA_PROPERTIES,\"event\":\"$EVENT_NAME\"}"
+  ENCODED_TRACK_DATA=`echo $TRACK_DATA | base64`
+  MIXPANEL_API_ENDPOINT="https://api.mixpanel.com/track/?data=$ENCODED_TRACK_DATA"
+  
+  if [ -n "$ENABLE_TRACKING" ]
+  then
+  	curl -s -X POST $MIXPANEL_API_ENDPOINT > /dev/null
+  fi
 }
 
 # Track the install of required command
 # Usage : track_command_install COMMAND METHOD STEP
 function track_command_install {
-track_event INSTALL "\"command\":\"$1\", \"method\":\"$2\",\"step\":\"$3\""
+  track_event INSTALL "\"command\":\"$1\", \"method\":\"$2\",\"step\":\"$3\""
 }
 
+function track_command_install_complete {
+  track_event INSTALL "\"command\":\"$1\", \"method\":\"$2\",\"step\":\"$3\""
+}
 # Function to grab the Mac Address of the local machine using a local Python install (This will work on MacOS/Linux but not Windows)
 function get_mac_address {
-echo `python -c "from uuid import getnode as get_mac; print get_mac()"`
+  echo `python -c "from uuid import getnode as get_mac; print get_mac()"`
 }
 
 # Routine to check whether an executable is already installed, if not provide a helpful error message and 
-function check_exec {
-  EXIT="echo 1"
+function check_exec() {
+  EXIT_CODE=0
   if [ -z "$2" ]
     then
       OUTPUT="$1"
@@ -73,9 +76,16 @@ function check_exec {
     then
       EXIT="$4"
     fi
-  command -v $1 >/dev/null 2>&1 || { echo "[Info] I require $OUTPUT but it's not installed. $HELP." >&2; track_event CHECK_EXEC "\"command\":\"$1\",\"installed\":\"false\""; $EXIT; }
-  track_event CHECK_EXEC "\"command\":\"$1\",\"installed\":\"true\""
-
+  command -v $1 >/dev/null 2>&1  || { 
+  		echo "[Info] I require $OUTPUT but it's not installed. $HELP." >&2
+		EXIT_CODE=1 
+	}
+  if [[ $EXIT_CODE == "1" ]]; then
+	  track_event CHECK_EXEC_NOT_INSTALLED "\"command\":\"$1\""
+  else
+	  track_event CHECK_EXEC_INSTALLED "\"command\":\"$1\""
+  fi
+  echo $EXIT_CODE
 }
 
 # Python3 Install Action with homebrew for MacOS
@@ -84,7 +94,7 @@ function do_install_python3_with_brew {
   track_command_install "python3" "brew" "start"
   brew update && brew install python3 
   track_command_install "python3" "brew" "complete"
-  track_event CHECK_EXEC "\"command\":\"python3\",\"installed\":\"true\""
+  track_event CHECK_EXEC_INSTALLED "\"command\":\"python3\""
 }
 
 # Python3 Install Action with yum for Redhat/Centos derivatives
@@ -92,7 +102,7 @@ function do_install_python3_with_yum {
   track_command_install "python3" "yum" "start"
   sudo yum install python3
   track_command_install "python3" "yum" "complete"
-  track_event CHECK_EXEC "\"command\":\"python3\",\"installed\":\"true\""
+  track_event CHECK_EXEC_INSTALLED "\"command\":\"python3\""
 }
 
 # Python3 Install Action with apt-get for Debian/Ubuntu derivatives
@@ -100,7 +110,7 @@ function do_install_python3_with_deb {
   track_command_install "python3" "apt-get" "start"
   sudo apt-get install python3
   track_command_install "python3" "apt-get" "complete"
-  track_event CHECK_EXEC "\"command\":\"python3\",\"installed\":\"true\""
+  track_event CHECK_EXEC_INSTALLED "\"command\":\"python3\""
 }
 
 # PIP Install Action
@@ -108,7 +118,7 @@ function do_install_pip {
   track_command_install "pip" "curl" "start"
   curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python3
   track_command_install "pip" "curl" "complete"
-  track_event CHECK_EXEC "\"command\":\"pip\",\"installed\":\"true\""
+  track_event CHECK_EXEC_INSTALLED "\"command\":\"pip\""
 }
 
 # Jasy Install Action
@@ -116,7 +126,7 @@ function do_install_or_update_jasy {
   track_command_install "jasy" "pip" "start"
   pip-3.2 install -U jasy
   track_command_install "jasy" "pip" "complete"
-  track_event CHECK_EXEC "\"command\":\"jasy\",\"installed\":\"true\""
+  track_event CHECK_EXEC_INSTALLED "\"command\":\"jasy\""
 }
 
 # Python3 Install with Brew
@@ -125,11 +135,11 @@ function check_python3_and_install {
   executable_pretty_print="python3"
   executable_action="Installing ..."
   result=`check_exec $executable $executable_pretty_print $executable_action`
-  if [[ $result == "0" ]]; then
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"false\""
+  if [[ $result == "1" ]]; then
+      track_event CHECK_EXEC_NOT_INSTALLED "\"command\":\"$executable\""
       do_install_python3_with_brew
     else
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"true\""
+      track_event CHECK_EXEC_INSTALLED "\"command\":\"$executable\""
       echo "[Info] Found $executable_pretty_print"
     fi
 }
@@ -140,11 +150,11 @@ function check_pip_and_install {
   executable_pretty_print="pip"
   executable_action="Installing ..."
   result=`check_exec $executable $executable_pretty_print $executable_action`
-  if [[ $result == "0" ]]; then
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"false\""
+  if [[ $result == "1" ]]; then
+      track_event CHECK_EXEC_NOT_INSTALLED "\"command\":\"$executable\""
       do_install_pip
     else
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"true\""
+      track_event CHECK_EXEC_INSTALLED "\"command\":\"$executable\""
       echo "[Info] Found $executable_pretty_print"
   fi
 }
@@ -155,11 +165,11 @@ function check_jasy_and_install {
   executable_pretty_print="jasy"
   executable_action="Installing ..."
   result=`check_exec $executable $executable_pretty_print $executable_action`
-  if [[ $result == "0" ]]; then
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"false\""
+  if [[ $result == "1" ]]; then
+      track_event CHECK_EXEC_NOT_INSTALLED "\"command\":\"$executable\""
       do_install_or_update_jasy
     else
-      track_event CHECK_EXEC "\"command\":\"$executable\",\"installed\":\"true\""
+      track_event CHECK_EXEC_INSTALLED "\"command\":\"$executable\""
       echo "[Info] Found $executable_pretty_print"
   fi
 }
